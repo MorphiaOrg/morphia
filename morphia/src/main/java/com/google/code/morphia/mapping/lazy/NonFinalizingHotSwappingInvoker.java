8fd55878c377805ef26be1b7e086140351fa35df
Uwe Schaefer <us@thomas-daily.de>
2010-07-22 18:51:50 +0000

    for someone could be as crazy as calling a method finalize(Object o) ;)

--- morphia/src/main/java/com/google/code/morphia/mapping/lazy/NonFinalizingHotSwappingInvoker.java
+++ morphia/src/main/java/com/google/code/morphia/mapping/lazy/NonFinalizingHotSwappingInvoker.java
@@ -18,8 +18,9 @@ class NonFinalizingHotSwappingInvoker extends HotSwappingInvoker {
 	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-		if ("finalize".equals(method.getName()))
+		if ("finalize".equals(method.getName()) && args != null && args.length == 0) {
 			return null;
+		}
 		
 		return super.invoke(proxy, method, args);
 	}
