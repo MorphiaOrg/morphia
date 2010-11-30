f8f42ab9507beddea4a8d00bddef57bce8f75528
Scott Hernandez <scotthernandez@gmail.com>
2010-11-30 15:15:33 +0000

    fixes issue 176

---
+++ morphia/src/test/java/com/google/code/morphia/issue176/TestIdTwice.java
@@ -0,0 +1,33 @@
+package com.google.code.morphia.issue176;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.google.code.morphia.TestBase;
+import com.google.code.morphia.annotations.Converters;
+import com.google.code.morphia.testutil.TestEntity;
+
+public class TestIdTwice extends TestBase {
+
+	@Test
+	public final void testCalendar() {
+		morphia.map(A.class);
+		A a = new A();
+		a.c = GregorianCalendar.getInstance();
+		ds.save(a);
+		A loaded = ds.find(A.class).get();
+		Assert.assertNotNull(loaded.c);
+		Assert.assertEquals(a.c, loaded.c);
+	}
+
+	@Converters(CalendarConverter.class)
+	private static class A extends TestEntity {
+		private static final long serialVersionUID = 1L;
+		Calendar c;
+	}
+
+}
