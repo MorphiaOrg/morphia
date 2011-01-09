5c08a6235db9353d9ff84a4de358c06227931a89
Uwe Schaefer <us@thomas-daily.de>
2011-01-09 17:54:04 +0000

    consistently changed name to Logr (even if i don´t like it :)

--- morphia/src/main/java/com/google/code/morphia/logging/MorphiaLogrFactory.java
+++ morphia/src/main/java/com/google/code/morphia/logging/MorphiaLogrFactory.java
@@ -12,7 +12,7 @@ public class MorphiaLogrFactory {
 	private static LogrFactory loggerFactory = null;
 	
 	private static List<String> factories = new ArrayList(Arrays.asList(JDKLoggerFactory.class.getName(),
-	"com.google.code.morphia.logging.slf4j.SLF4JLoggerImplFactory"));
+			"com.google.code.morphia.logging.slf4j.SLF4JLogrImplFactory"));
 	
 	private static synchronized void init() {
 		if (MorphiaLogrFactory.loggerFactory == null) {
@@ -24,11 +24,11 @@ public class MorphiaLogrFactory {
 		Collections.reverse(MorphiaLogrFactory.factories);
 		for (String f : MorphiaLogrFactory.factories) {
 			MorphiaLogrFactory.loggerFactory = newInstance(f);
-            if (MorphiaLogrFactory.loggerFactory != null) {
-                loggerFactory.get(MorphiaLogrFactory.class).info(
-                        "LoggerImplFactory set to " + loggerFactory.getClass().getName());
-                return;
-            }
+			if (MorphiaLogrFactory.loggerFactory != null) {
+				loggerFactory.get(MorphiaLogrFactory.class).info(
+						"LoggerImplFactory set to " + loggerFactory.getClass().getName());
+				return;
+			}
 		}
 		throw new IllegalStateException("Cannot instanciate any MorphiaLoggerFactory");
 	}
@@ -46,11 +46,11 @@ public class MorphiaLogrFactory {
 		init();
 		return MorphiaLogrFactory.loggerFactory.get(c);
 	}
-
-	/** Register a LoggerFactory; last one registered is used.**/
+	
+	/** Register a LoggerFactory; last one registered is used. **/
 	public static void registerLogger(Class<? extends LogrFactory> factoryClass) {
 		if (MorphiaLogrFactory.loggerFactory == null)
-			MorphiaLogrFactory.factories.add(0,factoryClass.getName());
+			MorphiaLogrFactory.factories.add(0, factoryClass.getName());
 		else
 			throw new IllegalStateException("LoggerImplFactory must be registered before logging is initialized.");
 	}
