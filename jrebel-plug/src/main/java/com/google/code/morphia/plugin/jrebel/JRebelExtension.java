e48882000c7e9fe7adab608365e07076c1dd8289
Uwe Schaefer <us@thomas-daily.de>
2011-02-20 13:06:34 +0000

    applied formatting

--- jrebel-plug/src/main/java/com/google/code/morphia/plugin/jrebel/JRebelExtension.java
+++ jrebel-plug/src/main/java/com/google/code/morphia/plugin/jrebel/JRebelExtension.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.google.code.morphia.plugin.jrebel;
+package com.google.code.morphia.ext.jrebel;
 
 import com.google.code.morphia.Morphia;
 import com.google.code.morphia.logging.Logr;
@@ -10,20 +10,15 @@ import com.google.code.morphia.logging.MorphiaLoggerFactory;
 /**
  * @author us@thomas-daily.de
  */
-public class JRebelExtension
-{
-    public JRebelExtension(final Morphia m)
-    {
-        final Logr log = MorphiaLoggerFactory.get(JRebelExtension.class);
-
-        if (JRebelPlugin.alreadyInstanciated)
-        {
-            new JRebelPlugin().applyTo(m);
-            log.info(this.getClass().getSimpleName() + " initialized");
-        }
-        else
-        {
-            log.error("JRebelPlugin was not initialized by JRebel. Class-remapping will not happen - the Extension is ignored.");
-        }
-    }
+public class JRebelExtension {
+	public JRebelExtension(final Morphia m) {
+		final Logr log = MorphiaLoggerFactory.get(JRebelExtension.class);
+		
+		if (JRebelPlugin.alreadyInstanciated) {
+			new JRebelPlugin().applyTo(m);
+			log.info(this.getClass().getSimpleName() + " initialized");
+		} else {
+			log.error("JRebelPlugin was not initialized by JRebel. Class-remapping will not happen - the Extension is ignored.");
+		}
+	}
 }
