5f3fb113f171aa4cea3d41de4f3caa6c2e2f91f1
Justin Lee <justin.lee@10gen.com>
2013-07-01 16:13:54 -0400

    add support for String[]

--- morphia/src/test/java/com/google/code/morphia/mapping/ListOfStringArrayMappingTest.java
+++ morphia/src/test/java/com/google/code/morphia/mapping/ListOfStringArrayMappingTest.java
@@ -2,6 +2,7 @@ package com.google.code.morphia.mapping;
 
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.junit.Assert;
@@ -16,7 +17,9 @@ import com.google.code.morphia.annotations.Id;
 public class ListOfStringArrayMappingTest extends TestBase {
   private static class ContainsListStringArray {
     @Id ObjectId id;
-    final ArrayList<String[]> listOfStrings = new ArrayList<String[]>();
+    final List<String[]> listOfStrings = new ArrayList<String[]>();
+    String[] arrayOfStrings;
+    String string;
   }
 
   @Test
@@ -24,9 +27,14 @@ public class ListOfStringArrayMappingTest extends TestBase {
     morphia.map(ContainsListStringArray.class);
     final ContainsListStringArray ent = new ContainsListStringArray();
     ent.listOfStrings.add(new String[] { "a", "b" });
+    ent.arrayOfStrings =  new String[] { "only", "the", "lonely" };
+    ent.string = "raw string";
+
     ds.save(ent);
     final ContainsListStringArray loaded = ds.get(ent);
     Assert.assertNotNull(loaded.id);
     Assert.assertArrayEquals(ent.listOfStrings.get(0), loaded.listOfStrings.get(0));
+    Assert.assertArrayEquals(ent.arrayOfStrings, loaded.arrayOfStrings);
+    Assert.assertEquals(ent.string, loaded.string);
   }
 }
