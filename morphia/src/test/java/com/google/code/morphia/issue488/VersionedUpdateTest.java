0ee1d210b42d48d8a704a88e13afffd5d6822508
Justin Lee <justin.lee@10gen.com>
2013-07-23 17:17:37 -0400

    rename class and test

--- morphia/src/test/java/com/google/code/morphia/issue488/VersionedUpdateTest.java
+++ morphia/src/test/java/com/google/code/morphia/issue488/VersionedUpdateTest.java
@@ -8,7 +8,7 @@ import com.google.code.morphia.TestBase;
 import com.google.code.morphia.annotations.Id;
 import com.google.code.morphia.annotations.Version;
 
-public class DataStoreImplVersionedUpdate extends TestBase {
+public class VersionedUpdateTest extends TestBase {
 
 	private static class TestEntity {
 		@Id
@@ -19,17 +19,17 @@ public class DataStoreImplVersionedUpdate extends TestBase {
 	}
 
 	@Test
-	public void itShouldUseCorrectIdForVersionedUpdate() {
+	public void versionedUpdate() {
 		TestEntity t = new TestEntity();
 		t.name = "foo";
-		
+
 		this.ds.save(t);
-		
+
 		TestEntity t1 = this.ds.get(TestEntity.class, t.id);
 		t1.name = "bar";
-		
+
 		this.ds.merge(t1);
-		
+
 		TestEntity t2 = this.ds.get(TestEntity.class, t.id);
 		Assert.assertEquals(t1.name, t2.name);
 	}
