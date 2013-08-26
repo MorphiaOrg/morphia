469ea3ebdeb088a23411ea8f7276053fa8bdda53
Justin Lee <justin.lee@10gen.com>
2013-08-26 16:38:50 -0400

    test validating that #62 works

--- morphia/src/test/java/com/google/code/morphia/TestQueriesOnReferences.java
+++ morphia/src/test/java/com/google/code/morphia/TestQueriesOnReferences.java
@@ -1,8 +1,8 @@
 package com.google.code.morphia;
 
 
+import org.bson.types.ObjectId;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import com.google.code.morphia.annotations.Entity;
 import com.google.code.morphia.annotations.Id;
@@ -10,7 +10,7 @@ import com.google.code.morphia.annotations.Reference;
 import com.google.code.morphia.query.Query;
 
 
-@Ignore //https://github.com/mongodb/morphia/issues/62
+//@Ignore //https://github.com/mongodb/morphia/issues/62
 public class TestQueriesOnReferences extends TestBase {
   @Entity
   public static class ContainsPic {
@@ -20,12 +20,25 @@ public class TestQueriesOnReferences extends TestBase {
     Pic pic;
     @Reference(lazy = true)
     Pic lazyPic;
+    @Reference(lazy = true)
+    PicWithObjectId lazyObjectIdPic;
   }
 
   @Entity
   public static class Pic {
     @Id
     String id;
+    String name;
+
+    public Pic() {
+      id = new ObjectId().toString();
+    }
+  }
+  
+  @Entity
+  public static class PicWithObjectId {
+    @Id
+    ObjectId id;
     String name;
   }
 
@@ -52,12 +65,20 @@ public class TestQueriesOnReferences extends TestBase {
     final ContainsPic cpk = new ContainsPic();
     final Pic p = new Pic();
     ds.save(p);
+    final PicWithObjectId withObjectId = new PicWithObjectId();
+    ds.save(withObjectId);
     cpk.lazyPic = p;
+    cpk.lazyObjectIdPic = withObjectId;
     ds.save(cpk);
 
-    final Query<ContainsPic> query = ds.createQuery(ContainsPic.class);
+    Query<ContainsPic> query = ds.createQuery(ContainsPic.class);
     Assert.assertNotNull(query.field("lazyPic")
       .equal(p)
+      .get());
+
+    query = ds.createQuery(ContainsPic.class);
+    Assert.assertNotNull(query.field("lazyObjectIdPic")
+      .equal(withObjectId)
       .get());
   }
 }
