0a6e337e445b880f88a3343bbf6db5629e90dc21
Justin Lee <justin.lee@10gen.com>
2013-11-18 12:34:21 +0100

    test $limit and $skip

--- morphia/src/test/java/org/mongodb/morphia/aggregation/AggregationTest.java
+++ morphia/src/test/java/org/mongodb/morphia/aggregation/AggregationTest.java
@@ -44,13 +44,49 @@ public class AggregationTest extends TestBase {
         Author author = aggregate.next();
         Assert.assertEquals("Homer", author.getName());
         Assert.assertEquals(Arrays.asList("The Odyssey", "Iliad"), author.getBooks());
-        
+
         getDs().createAggregation(Book.class, Author.class)
             .group("author", Group.grouping("books", Group.push("title")))
             .out("different")
             .aggregate();
 
         Assert.assertEquals(2, getDb().getCollection("different").count());
+    }
+
+    @Test
+    public void testLimit() {
+        getDs().save(new Book("The Banquet", "Dante", 2),
+                     new Book("Divine Comedy", "Dante", 1),
+                     new Book("Eclogues", "Dante", 2),
+                     new Book("The Odyssey", "Homer", 10),
+                     new Book("Iliad", "Homer", 10));
+
+        MorphiaIterator<Book, Book> aggregate = getDs().createAggregation(Book.class, Book.class)
+                                                    .limit(2)
+                                                    .aggregate();
+        int count = 0;
+        while (aggregate.hasNext()) {
+            aggregate.next();
+            count++;
+        }
+        Assert.assertEquals(2, count);
+    }
+
+    @Test
+    public void testSkip() {
+        getDs().save(new Book("The Banquet", "Dante", 2),
+                     new Book("Divine Comedy", "Dante", 1),
+                     new Book("Eclogues", "Dante", 2),
+                     new Book("The Odyssey", "Homer", 10),
+                     new Book("Iliad", "Homer", 10));
+
+        MorphiaIterator<Book, Book> aggregate = getDs().createAggregation(Book.class, Book.class)
+                                                    .skip(2)
+                                                    .aggregate();
+        Book book = aggregate.next();
+        Assert.assertEquals("Eclogues", book.getTitle());
+        Assert.assertEquals("Dante", book.getAuthor());
+        Assert.assertEquals(2, book.getCopies().intValue());
     }
 
     @Entity("books")
