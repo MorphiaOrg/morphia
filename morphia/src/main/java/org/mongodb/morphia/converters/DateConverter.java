6a405b43e00f40097feff8c9877d2d6aa4df94cc
nik <antonnik94@gmail.com>
2013-12-09 20:50:26 +0200

    issue558 - Resolved problem with storing Map with keys of Date type

--- morphia/src/main/java/org/mongodb/morphia/converters/DateConverter.java
+++ morphia/src/main/java/org/mongodb/morphia/converters/DateConverter.java
@@ -1,39 +1,52 @@
 package org.mongodb.morphia.converters;
 
-
-import org.mongodb.morphia.mapping.MappedField;
-
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.mongodb.morphia.logging.Logr;
+import org.mongodb.morphia.logging.MorphiaLoggerFactory;
+import org.mongodb.morphia.mapping.MappedField;
 
 /**
  * @author Uwe Schaefer, (us@thomas-daily.de)
  * @author scotthernandez
  */
 public class DateConverter extends TypeConverter implements SimpleValueConverter {
+	private static final Logr LOG = MorphiaLoggerFactory.get(DateConverter.class);
 
-    public DateConverter() {
-        this(Date.class);
-    }
+	public DateConverter() {
+		this(Date.class);
+	}
 
-    protected DateConverter(final Class clazz) {
-        super(clazz);
-    }
+	protected DateConverter(final Class clazz) {
+		super(clazz);
+	}
 
-    @Override
-    public Object decode(final Class targetClass, final Object val, final MappedField optionalExtraInfo) {
-        if (val == null) {
-            return null;
-        }
+	@Override
+	public Object decode(final Class targetClass, final Object val, final MappedField optionalExtraInfo) {
+		if (val == null) {
+			return null;
+		}
 
-        if (val instanceof Date) {
-            return val;
-        }
+		if (val instanceof Date) {
+			return val;
+		}
 
-        if (val instanceof Number) {
-            return new Date(((Number) val).longValue());
-        }
+		if (val instanceof Number) {
+			return new Date(((Number) val).longValue());
+		}
 
-        return new Date(Date.parse(val.toString())); // good luck
-    }
+		if (val instanceof String) {
+			DateFormat df = new SimpleDateFormat("EEE MMM dd kk:mm:ss z yyyy");
+			try {
+				return df.parse((String) val);
+			} catch (ParseException e) {
+				LOG.error("Can't unparse Date from String: " + val);
+			}
+		}
+
+		return new Date(Date.parse(val.toString())); // good luck
+	}
 }
