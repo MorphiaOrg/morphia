64923d4b0722aa413a61b398b299fec0c8eaf744
Justin Lee <justin.lee@10gen.com>
2014-02-18 16:55:15 -0500

    classpath and checkstyle fixes.

--- gwt/src/main/java/org/bson/types/ObjectId_CustomFieldSerializer.java
+++ gwt/src/main/java/org/bson/types/ObjectId_CustomFieldSerializer.java
@@ -5,38 +5,25 @@ import com.google.gwt.user.client.rpc.SerializationStreamReader;
 import com.google.gwt.user.client.rpc.SerializationStreamWriter;
 
 /**
- *  
+ *
  */
-public class ObjectId_CustomFieldSerializer {
-  /**
-   * @param streamReader
-   * @param instance
-   * @throws SerializationException
-   */
-  public static void deserialize(SerializationStreamReader streamReader, ObjectId instance)
-      throws SerializationException {
-    // already handled in instantiate
-  }
+//CHECKSTYLE:OFF
+public final class ObjectId_CustomFieldSerializer {
+    //CHECKSTYLE:ON    
+    private ObjectId_CustomFieldSerializer() {
+    }
 
-  /**
-   * @param streamReader
-   * @return
-   * @throws SerializationException
-   */
-  public static ObjectId instantiate(SerializationStreamReader streamReader)
-      throws SerializationException {
-    return new ObjectId(streamReader.readInt(), streamReader.readInt(), streamReader.readInt());
-  }
+    public static void deserialize(final SerializationStreamReader streamReader, final ObjectId instance) throws SerializationException {
+        // already handled in instantiate
+    }
 
-  /**
-   * @param streamWriter
-   * @param instance
-   * @throws SerializationException
-   */
-  public static void serialize(SerializationStreamWriter streamWriter, ObjectId instance)
-      throws SerializationException {
-    streamWriter.writeInt(instance._time);
-    streamWriter.writeInt(instance._machine);
-    streamWriter.writeInt(instance._inc);
-  }
+    public static ObjectId instantiate(final SerializationStreamReader streamReader) throws SerializationException {
+        return new ObjectId(streamReader.readInt(), streamReader.readInt(), streamReader.readInt());
+    }
+
+    public static void serialize(final SerializationStreamWriter streamWriter, final ObjectId instance) throws SerializationException {
+        streamWriter.writeInt(instance._time);
+        streamWriter.writeInt(instance._machine);
+        streamWriter.writeInt(instance._inc);
+    }
 }
