ca5fda09f1ad8793125ea169d65faa17e2e25859
Trisha Gee <trishagee@github.com>
2014-07-17 15:53:46 +0200

    [Release Automation] Documentation.  Final commit to finish the functionality that fixes #637

--- buildSrc/src/main/groovy/org/mongodb/morphia/release/ReleasePlugin.groovy
+++ buildSrc/src/main/groovy/org/mongodb/morphia/release/ReleasePlugin.groovy
@@ -4,7 +4,19 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.maven.MavenDeployment
 
-//requires nexus plugin
+/**
+ * Inspired originally by this <a href="https://github.com/trnl/github-release-gradle-plugin/">Release Plugin</a>.  This co-ordinates the
+ * tasks that make up the steps of Morphia's automated release process.
+ * 
+ * This uses the following plugins & libraries<ol>
+ * <li><a href="https://github.com/bmuschko/gradle-nexus-plugin">Nexus plugin</a></li>
+ * <li><a href="https://github.com/kohsuke/github-api">Github plugin</a></li>
+ * <li><a href="http://www.eclipse.org/jgit/download/">JGit</a></ol>
+ * 
+ * As such, you need to provide the configuration those tools need.  You'll need Nexus and Github credentials in 
+ * <code>~/.gradle/gradle.properties</code>, and Github credentials in <code>~/.github</code> - this duplication is because the two 
+ * git-related libraries get their configuration from different places.
+ */
 class ReleasePlugin implements Plugin<Project> {
     private Project project
 
