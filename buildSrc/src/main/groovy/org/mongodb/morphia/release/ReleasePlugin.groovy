0c9fef4e4ce525a9d59d24bb0e1fb586b18bc711
Trisha Gee <trishagee@github.com>
2014-07-17 18:45:52 +0200

    [Release Automation] Fixed uploadArchives for uploading snapshots

--- buildSrc/src/main/groovy/org/mongodb/morphia/release/ReleasePlugin.groovy
+++ buildSrc/src/main/groovy/org/mongodb/morphia/release/ReleasePlugin.groovy
@@ -7,12 +7,12 @@ import org.gradle.api.artifacts.maven.MavenDeployment
 /**
  * Inspired originally by this <a href="https://github.com/trnl/github-release-gradle-plugin/">Release Plugin</a>.  This co-ordinates the
  * tasks that make up the steps of Morphia's automated release process.
- * 
+ *
  * This uses the following plugins & libraries<ol>
  * <li><a href="https://github.com/bmuschko/gradle-nexus-plugin">Nexus plugin</a></li>
  * <li><a href="https://github.com/kohsuke/github-api">Github plugin</a></li>
  * <li><a href="http://www.eclipse.org/jgit/download/">JGit</a></ol>
- * 
+ *
  * As such, you need to provide the configuration those tools need.  You'll need Nexus and Github credentials in 
  * <code>~/.gradle/gradle.properties</code>, and Github credentials in <code>~/.github</code> - this duplication is because the two 
  * git-related libraries get their configuration from different places.
@@ -26,9 +26,9 @@ class ReleasePlugin implements Plugin<Project> {
         project.extensions.create('release', ReleasePluginExtension)
         project.evaluationDependsOnChildren()
 
-        project.task('prepareRelease', type: PrepareReleaseTask, dependsOn: project.subprojects.clean)
         magicIncantationRequiredToGetNexusPluginWorkingOnSubprojects()
-        project.task('draftReleaseNotes', type: DraftReleaseNotesTask, dependsOn: project.subprojects.uploadArchives)
+        project.task('prepareRelease', type: PrepareReleaseTask, dependsOn: project.subprojects.clean)
+        project.task('draftReleaseNotes', type: DraftReleaseNotesTask, dependsOn: ['prepareRelease', project.subprojects.uploadArchives])
         project.task('publishJavadoc', type: PublishJavadocTask, dependsOn: ['draftReleaseNotes', project.subprojects.javadoc])
         project.task('updateToNextVersion', type: UpdateToNextVersionTask, dependsOn: 'publishJavadoc')
         project.task('release', dependsOn: 'updateToNextVersion')
@@ -69,14 +69,13 @@ class ReleasePlugin implements Plugin<Project> {
                         beforeDeployment { MavenDeployment deployment -> signing.signPom(deployment) }
                     }
                 }
-            }.dependsOn(this.project.prepareRelease)
+            }.mustRunAfter(this.project.prepareRelease)
 
             modifyPom {
                 project {
                     name = 'Morphia'
                     description = 'Java Object Document Mapper for MongoDB'
                     url 'https://github.com/mongodb/morphia.git'
-                    version = project.release.releaseVersion
 
                     scm {
                         url 'https://github.com/mongodb/morphia.git'
