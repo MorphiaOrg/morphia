25395ace4a6e872a31a25d1e75ba9a399357a3b1
Eemeli Palotie <eemeli.palotie@gmail.com>
2014-09-01 12:44:37 +0300

    Revert "Updated syntax and tests according to Morphia spec."
    This reverts commit 6dbccf2e39925ec0b8b95ce8489976b27b619a8d.

--- morphia/src/test/java/org/mongodb/morphia/converters/LocaleConverterTest.java
+++ morphia/src/test/java/org/mongodb/morphia/converters/LocaleConverterTest.java
@@ -9,7 +9,6 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class LocaleConverterTest extends TestBase {
-
     @Test
     public void shouldEncodeAndDecodeBuiltInLocale() throws Exception {
         // given
@@ -37,57 +36,5 @@ public class LocaleConverterTest extends TestBase {
         assertThat(decodedLocale.getLanguage(), is("de"));
         assertThat(decodedLocale.getCountry(), is("DE"));
         assertThat(decodedLocale.getVariant(), is("bavarian"));
-    }
-
-    @Test
-    public void shouldEncodeAndDecodeCountyOnlyLocale() {
-        //given
-        LocaleConverter converter = new LocaleConverter();
-        Locale expectedLocale = new Locale("", "FI");
-
-        // when
-        Locale decodedLocale = (Locale) converter.decode(Locale.class, converter.encode(expectedLocale));
-
-        // then
-        assertThat(decodedLocale, is(expectedLocale));
-    }
-
-    @Test
-    public void shouldEncodeAndDecodeNoCountryLocale() {
-        //given
-        LocaleConverter converter = new LocaleConverter();
-        Locale expectedLocale = new Locale("fi", "", "VAR");
-
-        // when
-        Locale decodedLocale = (Locale) converter.decode(Locale.class, converter.encode(expectedLocale));
-
-        // then
-        assertThat(decodedLocale, is(expectedLocale));
-    }
-
-    @Test
-    public void shouldEncodeAndDecodeNoLanguageLocale() {
-        //given
-        LocaleConverter converter = new LocaleConverter();
-        Locale expectedLocale = new Locale("", "FI", "VAR");
-
-        // when
-        Locale decodedLocale = (Locale) converter.decode(Locale.class, converter.encode(expectedLocale));
-
-        // then
-        assertThat(decodedLocale, is(expectedLocale));
-    }
-
-    @Test
-    public void shouldEncodeAndDecodeSpecialVariantLocale() {
-        //given
-        LocaleConverter converter = new LocaleConverter();
-        Locale expectedLocale = new Locale("fi", "", "VAR");
-
-        // when
-        Locale decodedLocale = (Locale) converter.decode(Locale.class, converter.encode(expectedLocale));
-
-        // then
-        assertThat(decodedLocale, is(expectedLocale));
     }
 }
