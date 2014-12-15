920483d6d63c500cbff745406abb3e1f773957f5
Trisha Gee <trishagee@github.com>
2014-12-15 14:07:02 +0100

    [Issue 647] Fixed unit test.

--- morphia/src/test/java/org/mongodb/morphia/issue647/TestTypeCriteria.java
+++ morphia/src/test/java/org/mongodb/morphia/issue647/TestTypeCriteria.java
@@ -2,18 +2,13 @@ package org.mongodb.morphia.issue647;
 
 import org.bson.types.ObjectId;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.Morphia;
 import org.mongodb.morphia.TestBase;
 import org.mongodb.morphia.annotations.Entity;
 import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Property;
 import org.mongodb.morphia.query.Query;
 import org.mongodb.morphia.query.Type;
-
-import com.mongodb.MongoClient;
 
 public class TestTypeCriteria extends TestBase {
 
@@ -35,16 +30,15 @@ public class TestTypeCriteria extends TestBase {
 
     }
 
-    @Ignore
     @Test
     public void getStringTypeData() throws Exception {
+        Class1 entity = new Class1();
+        entity.firstName = "first_name";
+        getDs().save(entity);
+        
         getMorphia().map(Class1.class);
 
-        Morphia morphia =  getMorphia().map(Class1.class);
-        MongoClient mongoClient = new MongoClient("localhost");
-        Datastore ds = morphia.createDatastore(mongoClient, "test");
-
-        Query<Class1> query = ds.createQuery(Class1.class);
+        Query<Class1> query = getDs().createQuery(Class1.class);
         query.criteria("first_name").type(Type.STRING);
         Assert.assertTrue(query.asList().size() > 0);
     }
