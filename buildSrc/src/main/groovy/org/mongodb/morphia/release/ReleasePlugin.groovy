a843c7adc31493a66a057016ab8e9c294a32cf72
Trisha Gee <trishagee@github.com>
2014-12-17 11:12:10 +0100

    [Release Automation] Fixed uploadArchives to upload javadoc & sources and to correctly sign the artifacts.

--- buildSrc/src/main/groovy/org/mongodb/morphia/release/ReleasePlugin.groovy
+++ buildSrc/src/main/groovy/org/mongodb/morphia/release/ReleasePlugin.groovy
@@ -2,7 +2,6 @@ package org.mongodb.morphia.release
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.maven.MavenDeployment
 
 /**
  * Inspired originally by this <a href="https://github.com/trnl/github-release-gradle-plugin/">Release Plugin</a>.  This co-ordinates the
@@ -27,83 +26,12 @@ class ReleasePlugin implements Plugin<Project> {
         project.evaluationDependsOnChildren()
 
         project.task('prepareRelease', type: PrepareReleaseTask, dependsOn: project.subprojects.clean)
-        magicIncantationRequiredToGetNexusPluginWorkingOnSubprojects()
+        // uploadArchives is configured in publish.gradle
         project.task('draftReleaseNotes', type: DraftReleaseNotesTask, dependsOn: ['prepareRelease', project.subprojects.uploadArchives])
         project.task('publishJavadoc', type: PublishJavadocTask, dependsOn: ['draftReleaseNotes', project.subprojects.javadoc])
         project.task('updateToNextVersion', type: UpdateToNextVersionTask, dependsOn: 'publishJavadoc')
         project.task('release', dependsOn: 'updateToNextVersion')
     }
 
-    def magicIncantationRequiredToGetNexusPluginWorkingOnSubprojects() {
-        // feels like most of this shouldn't be needed as the repos etc are defaults, but they are required when you 
-        // add this as a dependency to a top-level project instead of just running it alone
-        project.subprojects { subproject ->
-            apply plugin: 'nexus'
-
-            project.ext.mavenDeployers = []
-
-            def pom = { config ->
-                project.mavenDeployers*.pom config
-            }
-
-            install {
-                project.mavenDeployers << repositories.mavenInstaller
-            }
-
-            uploadArchives {
-                project.mavenDeployers << repositories.mavenDeployer {
-                    def nexusCredentials = [
-                            userName: project.properties.nexusUsername,
-                            password: project.properties.nexusPassword
-                    ]
-
-                    snapshotRepository(url: 'https://oss.sonatype.org/content/repositories/snapshots') {
-                        authentication(nexusCredentials)
-                    }
-                    repository(url: 'https://oss.sonatype.org/service/local/staging/deploy/maven2/') {
-                        authentication(nexusCredentials)
-                    }
-                }
-                repositories {
-                    mavenDeployer {
-                        beforeDeployment { MavenDeployment deployment -> signing.signPom(deployment) }
-                    }
-                }
-            }.mustRunAfter(this.project.prepareRelease)
-
-            modifyPom {
-                project {
-                    name = 'Morphia'
-                    description = 'Java Object Document Mapper for MongoDB'
-                    url 'https://github.com/mongodb/morphia.git'
-
-                    scm {
-                        url 'https://github.com/mongodb/morphia.git'
-                        connection 'scm:git:https://github.com/mongodb/morphia.git'
-                    }
-
-                    licenses {
-                        license {
-                            name 'The Apache Software License, Version 2.0'
-                            url 'http://www.apache.org/licenses/LICENSE-2.0.txt'
-                            distribution 'repo'
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            name 'Various'
-                            organization = 'MongoDB'
-                        }
-                    }
-                }
-
-                whenConfigured { resultPom ->
-                    resultPom.dependencies.removeAll { dep -> dep.scope != 'compile' }
-                    resultPom.dependencies*.scope = null
-                }
-            }
-        }
-    }
 }
 
