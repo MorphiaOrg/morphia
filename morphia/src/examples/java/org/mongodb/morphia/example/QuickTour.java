ec1b48f7350718126ec80913f9e08e406e79e052
Justin Lee <justin.lee@10gen.com>
2015-05-01 16:43:12 -0400

    WIP

--- morphia/src/examples/java/org/mongodb/morphia/example/QuickTour.java
+++ morphia/src/examples/java/org/mongodb/morphia/example/QuickTour.java
@@ -1,9 +1,14 @@
 package org.mongodb.morphia.example;
 
 import com.mongodb.MongoClient;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.Morphia;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Reference;
 
 import java.net.UnknownHostException;
+import java.util.List;
 
 public final class QuickTour {
     private QuickTour() {
@@ -19,4 +24,17 @@ public final class QuickTour {
         // create the Datastore connecting to the database running on the default port on the local host
         morphia.createDatastore(new MongoClient(), "morphia_example");
     }
+}
+
+@Entity("employees")
+class Employee {
+    @Id
+    private ObjectId id;
+    private String name;
+    @Reference
+    private Employee manager;
+    @Reference
+    private List<Employee> directReports;
+    private Double salary;
+    
 }
