c37d285e90d8d774653a0c29dccc78b88c555236
Justin Lee <justin.lee@10gen.com>
2015-05-12 10:17:27 -0400

    fix urls

--- docs/landing/static/s/js/frontpage.js
+++ docs/landing/static/s/js/frontpage.js
@@ -26,7 +26,7 @@ function initializeJS() {
 };
 
 var toggleDownload = function() {
-  downloadLink = 'https://oss.sonatype.org/content/repositories/releases/org/mongodb/mongodb-driver-reactivestreams';
+  downloadLink = 'https://oss.sonatype.org/content/repositories/releases/org/mongodb/morphia';
   prefix = $('.distroPicker').prop('checked') ? "#maven" : "#gradle"
   releaseVersion = $('.releasePicker').selectpicker().val();
   activeVersion = $('.releasePicker option:selected').text();
