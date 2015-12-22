6820180caa78c20acc583d03ed07ecdeb79ce77b
Alessandro Gherardi <alessandro.gherardi@yahoo.com>
2015-12-21 20:25:24 -0700

    Fixed another checkstyle issue in test

--- morphia/src/test/java/org/mongodb/morphia/mapping/lazy/TestLazyCollectionReference.java
+++ morphia/src/test/java/org/mongodb/morphia/mapping/lazy/TestLazyCollectionReference.java
@@ -41,7 +41,7 @@ public class TestLazyCollectionReference extends ProxyTestBase {
         // read root entity from DB
         root = getDs().get(root);
         assertNotFetched(root.references);
- 
+
         // use the lazy collection
         Collection<ReferencedEntity> retrievedReferences = root.references;
         Assert.assertEquals(2, retrievedReferences.size());
