5dbcfdb6238dd66a51df914052238f13c3c0d7d0
Alessandro Gherardi <alessandro.gherardi@yahoo.com>
2016-05-06 09:30:01 -0600

    Added @IdGetter annotation for method that returns the ID. If invoked on lazy load proxy, extract the ID from the key

---
+++ morphia/src/main/java/org/mongodb/morphia/annotations/IdGetter.java
@@ -0,0 +1,17 @@
+package org.mongodb.morphia.annotations;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface IdGetter {
+
+}
--- morphia/src/main/java/org/mongodb/morphia/mapping/lazy/NonFinalizingHotSwappingInvoker.java
+++ morphia/src/main/java/org/mongodb/morphia/mapping/lazy/NonFinalizingHotSwappingInvoker.java
@@ -6,6 +6,9 @@ import com.thoughtworks.proxy.kit.ObjectReference;
 import com.thoughtworks.proxy.toys.delegate.DelegationMode;
 import com.thoughtworks.proxy.toys.hotswap.HotSwappingInvoker;
 
+import org.mongodb.morphia.mapping.lazy.proxy.EntityObjectReference;
+import org.mongodb.morphia.annotations.IdGetter;
+
 import java.lang.reflect.Method;
 
 
@@ -23,6 +26,19 @@ class NonFinalizingHotSwappingInvoker<T> extends HotSwappingInvoker<T> {
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
         if ("finalize".equals(method.getName()) && args != null && args.length == 0) {
             return null;
+        }
+
+        /*
+         * If the method being invoked is annotated with @IdGetter and the delegate reference is an EntityObjectReference,
+         * return the id of the EntityObjectReference's key. This allows us to return the referenced entity's id without
+         * fetching the entity from the datastore.
+         */
+        if (method.getAnnotation(IdGetter.class) != null) {
+            ObjectReference<Object> delegateReference = getDelegateReference();
+            if (delegateReference instanceof EntityObjectReference) {
+                EntityObjectReference entityObjectReference = (EntityObjectReference)delegateReference;
+                return entityObjectReference.__getKey().getId();
+            }
         }
 
         return super.invoke(proxy, method, args);
