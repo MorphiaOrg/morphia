c80648de0381ef1c490b797674ff3d003a38c861
Ulrich Cech <developer@cech-home.de>
2016-05-20 22:49:26 +0200

    fixed #924 Why not provide map package recursively?

--- morphia/src/test/java/org/mongodb/morphia/MorphiaTest.java
+++ morphia/src/test/java/org/mongodb/morphia/MorphiaTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.mongodb.morphia.mapping.MappedClass;
 import org.mongodb.morphia.testmappackage.SimpleEntity;
 import org.mongodb.morphia.testmappackage.testmapsubpackage.SimpleEntityInSubPackage;
+import org.mongodb.morphia.testmappackage.testmapsubpackage.testmapsubsubpackage.SimpleEntityInSubSubPackage;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -37,14 +38,14 @@ public class MorphiaTest extends TestBase {
 
         // then
         Collection<MappedClass> mappedClasses = morphia.getMapper().getMappedClasses();
-        Iterator<MappedClass> iterator = mappedClasses.iterator();
-        assertThat(mappedClasses.size(), is(2));
+        assertThat(mappedClasses.size(), is(3));
         Collection<Class<?>> classes = new ArrayList<Class<?>>();
         for (MappedClass mappedClass : mappedClasses) {
             classes.add(mappedClass.getClazz());
         }
         assertTrue(classes.contains(SimpleEntity.class));
         assertTrue(classes.contains(SimpleEntityInSubPackage.class));
+        assertTrue(classes.contains(SimpleEntityInSubSubPackage.class));
     }
 
 }
