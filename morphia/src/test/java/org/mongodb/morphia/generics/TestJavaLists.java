8633ea47519d7a6241b7b5e0bc83a28f696ee92c
Justin Lee <justin.lee@10gen.com>
2016-06-06 16:22:13 -0400

    test case for #876

---
+++ morphia/src/test/java/org/mongodb/morphia/generics/TestJavaLists.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2008-2016 MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mongodb.morphia.generics;
+
+import com.mongodb.BasicDBObject;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.junit.Test;
+import org.mongodb.morphia.TestBase;
+import org.mongodb.morphia.annotations.Id;
+
+import java.util.List;
+import java.util.Map;
+
+public class TestJavaLists extends TestBase {
+    @Test
+    public void jsonObjects() {
+        getMorphia().map(JsonList.class);
+        populate(new BasicDBObject(Document.parse("{\"jsonObject\": { \"array\": [{ \"foo\": \"bar\" }]},}")));
+        populate(new BasicDBObject(Document.parse("{\"jsonList\" : [ 1, \"string\", true, null ],}")));
+        populate(new BasicDBObject(Document.parse("{\"jsonList\" : [ {  \"foo\" : \"bar\" }],}")));
+    }
+
+    private void populate(final BasicDBObject jsonObject) {
+        getMorphia().fromDBObject(null, JsonList.class, jsonObject);
+    }
+}
+
+class JsonList {
+    @Id
+    private ObjectId id;
+    private List<Object> jsonList;
+    private Map<String, Object> jsonObject;
+}
