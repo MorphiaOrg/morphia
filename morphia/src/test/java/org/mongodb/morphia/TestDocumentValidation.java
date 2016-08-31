075b6c2b686ddca26b864560f8d95fc86f3e04cd
Justin Lee <justin.lee@10gen.com>
2016-08-31 15:42:38 -0400

    removed import

--- morphia/src/test/java/org/mongodb/morphia/TestDocumentValidation.java
+++ morphia/src/test/java/org/mongodb/morphia/TestDocumentValidation.java
@@ -33,7 +33,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
