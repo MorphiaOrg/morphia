ff9d7653e132d777652226866ca8e6f68d8c01c5
Justin Lee <justin.lee@10gen.com>
2016-08-31 15:35:27 -0400

    expand overwriteValidation()

--- morphia/src/test/java/org/mongodb/morphia/TestDocumentValidation.java
+++ morphia/src/test/java/org/mongodb/morphia/TestDocumentValidation.java
@@ -16,14 +16,28 @@
 
 package org.mongodb.morphia;
 
+import com.mongodb.MongoWriteException;
 import com.mongodb.WriteConcernException;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.CreateCollectionOptions;
+import com.mongodb.client.model.ValidationAction;
+import com.mongodb.client.model.ValidationLevel;
+import com.mongodb.client.model.ValidationOptions;
+import org.bson.Document;
 import org.junit.Test;
+import org.mongodb.morphia.annotations.Validation;
 import org.mongodb.morphia.entities.DocumentValidation;
 
 import java.util.Date;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+@SuppressWarnings("Since15")
 public class TestDocumentValidation extends TestBase {
     @Test
     public void createValidation() {
@@ -32,6 +46,7 @@ public class TestDocumentValidation extends TestBase {
 
         try {
             getDs().save(new DocumentValidation("John", 1, new Date()));
+            fail("Document should have failed validation");
         } catch (WriteConcernException e) {
             assertTrue(e.getMessage().contains("Document failed validation"));
         }
@@ -41,9 +56,52 @@ public class TestDocumentValidation extends TestBase {
     }
 
     @Test
-    public void createValidationTwice() {
+    public void overwriteValidation() {
+        Document validator = Document.parse("{ jelly : { $ne : 'rhubarb' } }");
+        ValidationOptions options = new ValidationOptions()
+            .validator(validator)
+            .validationLevel(ValidationLevel.MODERATE)
+            .validationAction(ValidationAction.ERROR);
+        MongoDatabase database = getMongoClient().getDatabase(TEST_DB_NAME);
+        database.getCollection("validation").drop();
+        database.createCollection("validation", new CreateCollectionOptions().validationOptions(options));
+
+        assertEquals(validator, getValidator(database));
+
+        Document rhubarb = new Document("jelly", "rhubarb").append("number", 20);
+        database.getCollection("validation").insertOne(new Document("jelly", "grape"));
+        try {
+            database.getCollection("validation").insertOne(rhubarb);
+            fail("Document should have failed validation");
+        } catch (MongoWriteException e) {
+            assertTrue(e.getMessage().contains("Document failed validation"));
+        }
+
         getMorphia().map(DocumentValidation.class);
         getDs().enableDocumentValidation();
+        assertEquals(Document.parse(DocumentValidation.class.getAnnotation(Validation.class).value()), getValidator(database));
+
+        try {
+            database.getCollection("validation").insertOne(rhubarb);
+        } catch (MongoWriteException e) {
+            assertFalse(e.getMessage().contains("Document failed validation"));
+        }
+
+        try {
+            getDs().save(new DocumentValidation("John", 1, new Date()));
+            fail("Document should have failed validation");
+        } catch (WriteConcernException e) {
+            assertTrue(e.getMessage().contains("Document failed validation"));
+        }
+
         getDs().enableDocumentValidation();
+    }
+
+    @SuppressWarnings("unchecked")
+    private Document getValidator(final MongoDatabase database) {
+        Document document = database.runCommand(new Document("listCollections", 1).append("filter", new Document("name", "validation")));
+        List<Document> firstBatch = (List<Document>) ((Document) document.get("cursor")).get("firstBatch");
+        Document o = (Document) firstBatch.get(0).get("options");
+        return (Document) o.get("validator");
     }
 }
