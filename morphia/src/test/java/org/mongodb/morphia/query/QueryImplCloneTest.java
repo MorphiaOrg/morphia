0f04fa4fee60e91d8f1bb09d2214a2147f5a51ce
Justin Lee <justin.lee@10gen.com>
2016-10-12 08:58:30 -0400

    clean up

--- morphia/src/test/java/org/mongodb/morphia/query/QueryImplCloneTest.java
+++ morphia/src/test/java/org/mongodb/morphia/query/QueryImplCloneTest.java
@@ -9,18 +9,10 @@ import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.logging.Logger;
 import org.mongodb.morphia.logging.MorphiaLoggerFactory;
 
-import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
-
-/**
- * @author doc
- */
 public class QueryImplCloneTest extends TestBase {
-    private static final Logger LOG = MorphiaLoggerFactory.get(QueryImplCloneTest.class);
-
-    private static final List<String> ALLOWED = Arrays.asList("cache", "query");
 
     @Test
     public void testQueryClone() throws Exception {
@@ -37,38 +29,6 @@ public class QueryImplCloneTest extends TestBase {
                                .order("a");
         q.disableValidation().filter("foo", "bar");
         Assert.assertEquals(q, q.cloneQuery());
-    }
-
-    private boolean sameState(final Query q1, final Query q2) throws IllegalAccessException {
-        return sameState(q1.getClass(), q1, q2);
-    }
-
-    private boolean sameState(final Class c, final Query q1, final Query q2) throws IllegalAccessException {
-
-        final Field[] fields = c.getDeclaredFields();
-        for (final Field f : fields) {
-            f.setAccessible(true);
-
-            final Object v1 = f.get(q1);
-            final Object v2 = f.get(q2);
-
-            LOG.debug("checking field " + f.getName() + " v1=" + v1 + " v2=" + v2);
-
-            if (v1 == null && v2 == null) {
-                continue;
-            }
-
-            if (v1 != null && v1.equals(v2)) {
-                continue;
-            }
-
-            if (!ALLOWED.contains(f.getName())) {
-                throw new RuntimeException(f.getName() + " v1=" + v1 + " v2=" + v2);
-            }
-        }
-
-        final Class superclass = c.getSuperclass();
-        return (superclass == null || sameState(superclass, q1, q2));
     }
 
     static class E1 {
