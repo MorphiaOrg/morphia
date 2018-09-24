540717f3d22241601e010c4c58a58a881f9fa193
Vladimir L <vladimir.lioubitelev@gmail.com>
2018-09-24 13:37:31 +0200

    Fix for ensureIndexes() logic (#1211)
    * added integration tests to confirm the issue #1175
    
    * collect indexes from subclasses only if field is an interface or abstract class
    
    * fixed errors reported by checkstyle

---
+++ morphia/src/test/java/org/mongodb/morphia/issue1175/EnsureIndexesTest.java
@@ -0,0 +1,91 @@
+package org.mongodb.morphia.issue1175;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mongodb.morphia.testutil.IndexMatcher.doesNotHaveIndexNamed;
+import static org.mongodb.morphia.testutil.IndexMatcher.hasIndexNamed;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+import org.mongodb.morphia.TestBase;
+import org.mongodb.morphia.annotations.Embedded;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Indexed;
+import org.mongodb.morphia.utils.IndexDirection;
+
+import com.mongodb.DBObject;
+
+public class EnsureIndexesTest extends TestBase {
+    @Test
+    public final void ensureIndexesFromSuperclasses() {
+        // given
+        getMorphia().map(Person.class, Employee.class, Company.class);
+
+        // when
+        getAds().ensureIndexes();
+
+        // then
+        List<DBObject> indexInfo = getDs().getCollection(Company.class).getIndexInfo();
+        assertEquals(4, indexInfo.size());
+        assertThat(indexInfo, hasIndexNamed("_id_"));
+        assertThat(indexInfo, hasIndexNamed("employees.birthday_-1"));
+        assertThat(indexInfo, hasIndexNamed("employees.fullName_1"));
+        assertThat(indexInfo, hasIndexNamed("employees.employeeId_1"));
+    }
+
+    @Test
+    public final void ensureIndexesWithoutSubclasses() {
+        // given
+        getMorphia().map(Person.class, Employee.class, Contract.class);
+
+        // when
+        getAds().ensureIndexes();
+
+        // then
+        List<DBObject> indexInfo = getDs().getCollection(Contract.class).getIndexInfo();
+        assertEquals(3, indexInfo.size());
+        assertThat(indexInfo, hasIndexNamed("_id_"));
+        assertThat(indexInfo, hasIndexNamed("person.birthday_-1"));
+        assertThat(indexInfo, hasIndexNamed("person.fullName_1"));
+        assertThat(indexInfo, doesNotHaveIndexNamed("person.employeeId_1"));
+    }
+
+    @Embedded
+    public static class LivingBeing {
+        @Indexed(IndexDirection.DESC)
+        private Date birthday;
+    }
+
+    @Embedded
+    public static class Person extends LivingBeing {
+        @Indexed
+        private String fullName;
+    }
+
+    @Embedded
+    public static class Employee extends Person {
+        @Indexed
+        private Long employeeId;
+        private String title;
+    }
+
+    @Entity
+    public static class Contract {
+        @Id
+        private Long contractId;
+        private Long companyId;
+        @Embedded
+        private Person person;
+    }
+
+    @Entity
+    public static class Company {
+        @Id
+        private Long companyId;
+        @Embedded
+        private List<Employee> employees;
+    }
+}
