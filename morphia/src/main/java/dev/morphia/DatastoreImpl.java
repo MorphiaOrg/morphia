bb8999a9bfc590d039362a5e5a31b446310ed194
evanchooly <jlee@antwerkz.com>
2019-05-22 09:50:31 -0500

    the query might return nothing and so there will be nothing to map.  bail early if so.
    fixes #1344
    
    (cherry picked from commit 8f44b5865793dce67a9bb66ccf70b7a392a95bd5)

--- morphia/src/main/java/dev/morphia/DatastoreImpl.java
+++ morphia/src/main/java/dev/morphia/DatastoreImpl.java
@@ -333,7 +333,7 @@ public class DatastoreImpl implements AdvancedDatastore {
                                                                            .update(((UpdateOpsImpl<T>) operations).getOps())
                                                                            .getOptions());
 
-        return mapper.fromDBObject(this, query.getEntityClass(), res, createCache());
+        return res == null ? null : mapper.fromDBObject(this, query.getEntityClass(), res, createCache());
 
     }
 
