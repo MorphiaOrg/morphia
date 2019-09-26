c937705ee09f235bf8c5c9850fcc1b901ece6475
evanchooly <jlee@antwerkz.com>
2019-09-25 22:11:35 -0400

    deprecate @NotSaved
    fixes #1376

--- morphia/src/main/java/dev/morphia/annotations/NotSaved.java
+++ morphia/src/main/java/dev/morphia/annotations/NotSaved.java
@@ -14,6 +14,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE})
+@Deprecated
 public @interface NotSaved {
 
 }
