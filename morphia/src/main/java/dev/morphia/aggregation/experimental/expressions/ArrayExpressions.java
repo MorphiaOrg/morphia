b5bc2e5f4d97f729123dd581bb86a70a1eaf30eb
evanchooly <jlee@antwerkz.com>
2020-01-08 00:35:32 -0500

    update docs

--- morphia/src/main/java/dev/morphia/aggregation/experimental/expressions/ArrayExpressions.java
+++ morphia/src/main/java/dev/morphia/aggregation/experimental/expressions/ArrayExpressions.java
@@ -8,6 +8,14 @@ import java.util.List;
 
 public class ArrayExpressions {
 
+    /**
+     * Returns the element at the specified array index.
+     *
+     * @param array the array to use
+     * @param index the index to return
+     * @return the new expression
+     * @mongodb.driver.manual manual/reference/operator/aggregation/arrayElemAt $arrayElemAt
+     */
     public static Expression elementAt(final Expression array, final Expression index) {
         return new Expression("$arrayElemAt", List.of(array, index)) {
             @Override
