1fdb0a4cdac9524bc41057839df5c04064f86ecc
evanchooly <jlee@antwerkz.com>
2020-06-29 23:02:35 -0400

    someday i'll get it right.

--- travis-jobs.sh
+++ travis-jobs.sh
@@ -2,15 +2,11 @@
 
 cat .travis.template.yml > .travis.yml
 
-for URL in \
-  https://repo.mongodb.org/apt/ubuntu/dists/xenial/mongodb-org/4.2/multiverse/binary-amd64/mongodb-org-server_4.2.8_amd64.deb \
-  https://repo.mongodb.org/apt/ubuntu/dists/xenial/mongodb-org/4.0/multiverse/binary-amd64/mongodb-org-server_4.0.19_amd64.deb \
-  https://repo.mongodb.org/apt/ubuntu/dists/xenial/mongodb-org/3.6/multiverse/binary-amd64/mongodb-org-server_3.6.18_amd64.deb \
-  https://repo.mongodb.org/apt/ubuntu/dists/xenial/mongodb-org/3.4/multiverse/binary-amd64/mongodb-org-server_3.4.24_amd64.deb
+for MONGODB in 4.2.8 4.0.19 3.6.18 3.4.24
 do
   for DRIVER in 4.0.4 3.12.5 3.11.2 3.10.2 3.9.1 3.8.2 3.7.1
   do
-    MONGODB=$(echo "$URL" | grep -o -E '_[0-9]+\.[0-9]+\.[0-9]+_' | sed -e s/_//g )
-    echo "  - MONGODB=$MONGODB DRIVER=$DRIVER URL=$URL" >> .travis.yml
+    echo "  - MONGODB=$MONGODB DRIVER=$DRIVER" | tee -a .travis.yml
+    [ -e /tmp/mongodb-${MONGODB}.tgz ] || wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-${MONGODB}.tgz -O /tmp/mongodb-${MONGODB}.tgz
   done
 done
