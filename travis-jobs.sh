464f48dcd0d572d6d1cf54767e2977b108c797e9
evanchooly <jlee@antwerkz.com>
2020-06-29 22:17:42 -0400

    update travis versions.  automate building the matrix

---
+++ travis-jobs.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+cat .travis.template.yml > .travis.yml
+
+for MONGODB in 4.2.8 4.0.19 3.6.18 3.4.24
+do
+  for DRIVER in 4.0.4 3.12.5 3.11.2 3.10.2 3.9.1 3.8.2 3.7.1
+  do
+    echo "  - MONGODB=$MONGODB DRIVER=$DRIVER" >> .travis.yml
+  done
+done
