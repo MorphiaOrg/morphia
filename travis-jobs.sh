5c529d016f14ed46331118e805016380829f7bdd
evanchooly <jlee@antwerkz.com>
2020-06-29 23:07:07 -0400

    someday i'll get it right.

--- travis-jobs.sh
+++ travis-jobs.sh
@@ -6,7 +6,7 @@ for MONGODB in 4.2.8 4.0.19 3.6.18 3.4.24
 do
   for DRIVER in 4.0.4 3.12.5 3.11.2 3.10.2 3.9.1 3.8.2 3.7.1
   do
-    echo "  - MONGODB=$MONGODB DRIVER=$DRIVER" | tee -a .travis.yml
+    echo "  - MONGODB=$MONGODB DRIVER=$DRIVER" >> .travis.yml
     [ -e /tmp/mongodb-${MONGODB}.tgz ] || wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-${MONGODB}.tgz -O /tmp/mongodb-${MONGODB}.tgz
   done
 done
