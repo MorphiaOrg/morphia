1e44f6988c6175e81c66bb04311862f8188c1daa
evanchooly <jlee@antwerkz.com>
2020-07-30 00:51:27 -0400

    update travis config

--- travis-jobs.sh
+++ travis-jobs.sh
@@ -2,7 +2,7 @@
 
 cat .travis.template.yml > .travis.yml
 
-for MONGODB in 4.2.8 4.0.19 3.6.18 3.4.24
+for MONGODB in 4.2.8 4.0.19 3.6.18
 do
   for DRIVER in 4.0.4 3.12.5 3.11.2 3.10.2 3.9.1 3.8.2 3.7.1
   do
