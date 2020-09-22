f226799541728cc6e22e61a3e669ec74d8d19806
evanchooly <jlee@antwerkz.com>
2020-09-22 15:17:00 -0400

    document the expression

--- morphia/src/main/java/dev/morphia/aggregation/experimental/expressions/impls/FunctionExpression.java
+++ morphia/src/main/java/dev/morphia/aggregation/experimental/expressions/impls/FunctionExpression.java
@@ -7,11 +7,21 @@ import org.bson.codecs.EncoderContext;
 import static dev.morphia.aggregation.experimental.codecs.ExpressionHelper.document;
 import static dev.morphia.aggregation.experimental.codecs.ExpressionHelper.writeNamedValue;
 
+/**
+ * @since 2.1
+ */
 public class FunctionExpression extends Expression {
     private final String body;
     private final Expression[] args;
     private final String lang = "js";
 
+    /**
+     * Creates the new expression
+     *
+     * @param body the function definition
+     * @param args the funcation arguments
+     * @morphia.internal
+     */
     public FunctionExpression(String body, Expression[] args) {
         super("$function");
         this.body = body;
