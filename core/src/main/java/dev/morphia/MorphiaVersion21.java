2b71386f4f2b9c0abe4284e19eb3eb5f11100270
evanchooly <jlee@antwerkz.com>
2020-11-09 21:50:33 -0500

    Add Version Class to judge Morphia Version
    fixes #1496

---
+++ core/src/main/java/dev/morphia/MorphiaVersion21.java
@@ -0,0 +1,10 @@
+package dev.morphia;
+
+/**
+ * This class is a version marker for certain frameworks to use when loading configurations, e.g.  See
+ * {@link https://github.com/MorphiaOrg/morphia/issues/1496} for details.
+ */
+final class MorphiaVersion21 {
+    private MorphiaVersion21() {
+    }
+}
---
+++ core/src/test/java/dev/morphia/MorphiaVersionTest.java
@@ -0,0 +1,23 @@
+package dev.morphia;
+
+import com.github.zafarkhaja.semver.Version;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+
+import static java.lang.String.format;
+import static org.testng.Assert.assertEquals;
+
+public class MorphiaVersionTest {
+    @Test
+    public void testVersion() throws Exception {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("../pom.xml"));
+
+        Version version = Version.valueOf(model.getVersion());
+        String minorVersion = format("%s%s", version.getMajorVersion(), version.getMinorVersion());
+        assertEquals(MorphiaVersion21.class.getSimpleName().replaceAll("\\D", ""), minorVersion);
+    }
+}
