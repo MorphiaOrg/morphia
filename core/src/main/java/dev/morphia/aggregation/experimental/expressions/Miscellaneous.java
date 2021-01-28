52f7592be2271946d57dcc0891ec2b555d24d862
evanchooly <jlee@antwerkz.com>
2021-01-28 17:40:51 -0500

    clean up javadoc

--- core/src/main/java/dev/morphia/aggregation/experimental/expressions/Miscellaneous.java
+++ core/src/main/java/dev/morphia/aggregation/experimental/expressions/Miscellaneous.java
@@ -5,7 +5,15 @@ import dev.morphia.query.experimental.filters.Filter;
 import org.bson.BsonWriter;
 import org.bson.codecs.EncoderContext;
 
-public class Miscellaneous {
+/**
+ * Defines miscellaneous operators for aggregations.
+ *
+ * @since 2.2
+ */
+public final class Miscellaneous {
+    private Miscellaneous() {
+    }
+
     /**
      * Matches a random selection of input documents. The number of documents selected approximates the sample rate expressed as a
      * percentage of the total number of documents.
