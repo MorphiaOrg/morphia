307b338383437f27514fbfb36b522883d7a1f176
Renat Vasilyev <renat.vasilyev@nosto.com>
2021-03-24 11:33:23 +0200

    Checkstyle + better alignment between lifecycle and non-lifecycle entities

--- core/src/main/java/dev/morphia/mapping/codec/pojo/LifecycleDecoder.java
+++ core/src/main/java/dev/morphia/mapping/codec/pojo/LifecycleDecoder.java
@@ -1,13 +1,17 @@
 package dev.morphia.mapping.codec.pojo;
 
+import static java.lang.String.format;
+
+import org.bson.BsonReader;
+import org.bson.Document;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.configuration.CodecConfigurationException;
+
 import dev.morphia.annotations.PostLoad;
 import dev.morphia.annotations.PreLoad;
 import dev.morphia.mapping.codec.MorphiaInstanceCreator;
 import dev.morphia.mapping.codec.reader.DocumentReader;
-import org.bson.BsonReader;
-import org.bson.Document;
-import org.bson.codecs.DecoderContext;
-import org.bson.codecs.configuration.CodecConfigurationException;
 
 /**
  * @morphia.internal
@@ -31,13 +35,13 @@ public class LifecycleDecoder extends EntityDecoder {
         if (model.useDiscriminator()) {
             String discriminator = document.getString(model.getDiscriminatorKey());
             if (discriminator != null) {
-                try {
-                    Class<?> discriminatorClass = getMorphiaCodec().getDiscriminatorLookup().lookup(discriminator);
-                    model = getMorphiaCodec().getMapper().getEntityModel(discriminatorClass);
-                    // create morphia codec that corresponds to entity model
-                    getMorphiaCodec().getRegistry().get(discriminatorClass);
-                } catch (CodecConfigurationException e) {
-                    // Ignore missing discriminator class, use default entity model
+                Class<?> discriminatorClass = getMorphiaCodec().getDiscriminatorLookup().lookup(discriminator);
+                // need to load the codec to initialize cachedCodecs in field models
+                Codec<?> codec = getMorphiaCodec().getRegistry().get(discriminatorClass);
+                if (codec instanceof MorphiaCodec) {
+                    model = ((MorphiaCodec<?>) codec).getEntityModel();
+                } else {
+                    throw new CodecConfigurationException(format("Non-entity class used as discriminator: '%s'.", discriminator));
                 }
             }
         }
--- core/src/test/java/dev/morphia/test/mapping/codec/pojo/DiscriminantLifecycleTest.java
+++ core/src/test/java/dev/morphia/test/mapping/codec/pojo/DiscriminantLifecycleTest.java
@@ -1,7 +1,10 @@
 package dev.morphia.test.mapping.codec.pojo;
 
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecConfigurationException;
 import org.bson.types.ObjectId;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import dev.morphia.annotations.Entity;
@@ -11,21 +14,56 @@ import dev.morphia.annotations.Transient;
 import dev.morphia.query.experimental.filters.Filters;
 import dev.morphia.test.TestBase;
 
+/**
+ * asserts some common behaviour between entities with or without lifecycle
+ */
 public class DiscriminantLifecycleTest extends TestBase {
 
-    @Test
-    public void testEntity() {
-        ChildEntity childEntity = new ChildEntity();
-        childEntity.value = "value";
-        getDs().save(childEntity);
-        ObjectId id = childEntity.id;
-        BaseEntity saved = getDs().find(BaseEntity.class).filter(Filters.eq("_id", id)).first();
-        Assert.assertTrue(saved instanceof ChildEntity);
-        Assert.assertTrue(saved.audited);
+    @DataProvider(name = "classes")
+    public Object[][] classes() {
+        return new Object[][] {
+                {BaseLifecycleEntity.class, ChildLifecycleEntity.class},
+                {BaseEntity.class, ChildEntity.class}
+        };
+    }
+
+    @Test(dataProvider = "classes")
+    public void testCorrectEntity(Class<?> baseClass, Class<?> childClass) {
+        ObjectId id = saveChildEntity(childClass);
+        Child saved = (Child) getDs().find(baseClass).filter(Filters.eq("_id", id)).first();
+        Assert.assertTrue(childClass.isInstance(saved));
+        Assert.assertTrue(saved.getAudited());
+        Assert.assertEquals("embedded", saved.getEmbed().embeddedValue);
+    }
+
+    @Test(expectedExceptions = CodecConfigurationException.class, dataProvider = "classes")
+    public void testWrongDiscriminator(Class<?> baseClass, Class<?> childClass) {
+        Document entity = new Document("_t", "Nonsense");
+        ObjectId id = getDatabase().getCollection("entity").insertOne(entity).getInsertedId().asObjectId().getValue();;
+        getDs().find(baseClass).filter(Filters.eq("_id", id)).first();
+    }
+
+    @Test(expectedExceptions = CodecConfigurationException.class, dataProvider = "classes")
+    public void testNonEntityDiscriminator(Class<?> baseClass, Class<?> childClass) {
+        ObjectId id = saveChildEntity(NonEntity.class);
+        getDs().find(baseClass).filter(Filters.eq("_id", id)).first();
+    }
+
+    private ObjectId saveChildEntity(Class<?> entityClass) {
+        Document entity = new Document("_t", entityClass.getName());
+        Document embed = new Document("_t", Embed.class.getName());
+        embed.put("embeddedValue", "embedded");
+        entity.put("embed", embed);
+        return getDatabase().getCollection("entity").insertOne(entity).getInsertedId().asObjectId().getValue();
+    }
+
+    interface Child {
+        boolean getAudited();
+        Embed getEmbed();
     }
 
     @Entity(value = "entity")
-    static class BaseEntity{
+    static class BaseLifecycleEntity {
         @Id
         ObjectId id;
         @Transient
@@ -38,7 +76,59 @@ public class DiscriminantLifecycleTest extends TestBase {
         }
     }
 
-    static class ChildEntity extends BaseEntity {
-        String value;
+    static class ChildLifecycleEntity extends BaseLifecycleEntity implements Child{
+        Embed embed;
+
+        @Override
+        public boolean getAudited() {
+            return audited;
+        }
+
+        @Override
+        public Embed getEmbed() {
+            return embed;
+        }
+    }
+
+    @Entity(value = "entity")
+    static class BaseEntity {
+        @Id
+        ObjectId id;
+        @Transient
+        boolean audited = true;
+    }
+
+    static class ChildEntity extends BaseEntity implements Child{
+        Embed embed;
+
+        @Override
+        public boolean getAudited() {
+            return audited;
+        }
+
+        @Override
+        public Embed getEmbed() {
+            return embed;
+        }
+    }
+
+
+    @Entity
+    static class Embed {
+        String embeddedValue;
+    }
+
+    static class NonEntity implements Child {
+        Embed embed;
+
+        @Override
+        public boolean getAudited() {
+            return true;
+        }
+
+        @Override
+        public Embed getEmbed() {
+            return embed;
+        }
     }
 }
