1566461688b582436b359f9979b5c61acb199c0f
Renat Vasilyev <renat.vasilyev@nosto.com>
2021-03-25 03:48:09 +0200

    #1586 deserialization of polymorphic entities with lifecycle (#1587)
    fixes #1586 deserialization of polymorphic entities with lifecycle

--- core/src/main/java/dev/morphia/mapping/codec/pojo/LifecycleDecoder.java
+++ core/src/main/java/dev/morphia/mapping/codec/pojo/LifecycleDecoder.java
@@ -1,12 +1,17 @@
 package dev.morphia.mapping.codec.pojo;
 
+import static java.lang.String.format;
+
+import org.bson.BsonReader;
+import org.bson.Document;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.configuration.CodecConfigurationException;
+
 import dev.morphia.annotations.PostLoad;
 import dev.morphia.annotations.PreLoad;
 import dev.morphia.mapping.codec.MorphiaInstanceCreator;
 import dev.morphia.mapping.codec.reader.DocumentReader;
-import org.bson.BsonReader;
-import org.bson.Document;
-import org.bson.codecs.DecoderContext;
 
 /**
  * @morphia.internal
@@ -25,13 +30,25 @@ public class LifecycleDecoder extends EntityDecoder {
 
     @Override
     public Object decode(BsonReader reader, DecoderContext decoderContext) {
-        final MorphiaInstanceCreator instanceCreator = getInstanceCreator();
-        Object entity = instanceCreator.getInstance();
-        EntityModel model = getMorphiaCodec().getEntityModel();
         Document document = getMorphiaCodec().getRegistry().get(Document.class).decode(reader, decoderContext);
-
+        EntityModel model = getMorphiaCodec().getEntityModel();
+        if (model.useDiscriminator()) {
+            String discriminator = document.getString(model.getDiscriminatorKey());
+            if (discriminator != null) {
+                Class<?> discriminatorClass = getMorphiaCodec().getDiscriminatorLookup().lookup(discriminator);
+                // need to load the codec to initialize cachedCodecs in field models
+                Codec<?> codec = getMorphiaCodec().getRegistry().get(discriminatorClass);
+                if (codec instanceof MorphiaCodec) {
+                    model = ((MorphiaCodec<?>) codec).getEntityModel();
+                } else {
+                    throw new CodecConfigurationException(format("Non-entity class used as discriminator: '%s'.", discriminator));
+                }
+            }
+        }
+        final MorphiaInstanceCreator instanceCreator = model.getInstanceCreator();
+        Object entity = instanceCreator.getInstance();
         model.callLifecycleMethods(PreLoad.class, entity, document, getMorphiaCodec().getMapper());
-        decodeProperties(new DocumentReader(document), decoderContext, instanceCreator);
+        decodeProperties(new DocumentReader(document), decoderContext, instanceCreator, model);
         model.callLifecycleMethods(PostLoad.class, entity, document, getMorphiaCodec().getMapper());
 
         return entity;
