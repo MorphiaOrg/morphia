e6ce5ca830d2cef664aa1f24d26a6797400437e9
evanchooly <jlee@antwerkz.com>
2021-03-26 22:45:07 -0400

    byte[] saved format changed
    fixes #1557

---
+++ core/src/main/java/dev/morphia/mapping/codec/ByteWrapperArrayCodec.java
@@ -0,0 +1,44 @@
+package dev.morphia.mapping.codec;
+
+import org.bson.BsonBinary;
+import org.bson.BsonReader;
+import org.bson.BsonWriter;
+import org.bson.codecs.Codec;
+import org.bson.codecs.DecoderContext;
+import org.bson.codecs.EncoderContext;
+
+public class ByteWrapperArrayCodec implements Codec<Byte[]> {
+    @Override
+    public Byte[] decode(BsonReader reader, DecoderContext decoderContext) {
+        return wrapper(reader.readBinaryData().getData());
+    }
+
+    @Override
+    public void encode(BsonWriter writer, Byte[] value, EncoderContext encoderContext) {
+        writer.writeBinaryData(new BsonBinary(primitive(value)));
+    }
+
+    @Override
+    public Class<Byte[]> getEncoderClass() {
+        return Byte[].class;
+    }
+
+    private byte[] primitive(Byte[] value) {
+        byte[] array = new byte[value.length];
+        for (int i = 0; i < value.length; i++) {
+            array[i] = value[i];
+        }
+
+        return array;
+    }
+
+    private Byte[] wrapper(byte[] value) {
+        Byte[] array = new Byte[value.length];
+        for (int i = 0; i < value.length; i++) {
+            array[i] = value[i];
+        }
+
+        return array;
+    }
+
+}
