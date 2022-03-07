a33f07840068ad5ca3d93d4cf98a3e1798755b7c
evanchooly <jlee@antwerkz.com>
2022-03-07 16:02:15 -0500

    bucket of nope

--- core/src/test/java/dev/morphia/test/aggregation/experimental/TestAggregation.java
+++ core/src/test/java/dev/morphia/test/aggregation/experimental/TestAggregation.java
@@ -665,37 +665,37 @@ public class TestAggregation extends TestBase {
         checkMinServerVersion(5.0);
 
         insert("inventory", parseDocs(
-            "{ '_id' : 1, 'item' : 'sweatshirt', 'price.usd': 45.99, qty: 300 }",
-            "{ '_id' : 2, 'item' : 'winter coat', 'price.usd': 499.99, qty: 200 }",
-            "{ '_id' : 3, 'item' : 'sun dress', 'price.usd': 199.99, qty: 250 }",
-            "{ '_id' : 4, 'item' : 'leather boots', 'price.usd': 249.99, qty: 300 }",
-            "{ '_id' : 5, 'item' : 'bow tie', 'price.usd': 9.99, qty: 180 }"));
+            "{ '_id' : 1, 'item' : 'sweatshirt', 'price_usd': 45.99, qty: 300 }",
+            "{ '_id' : 2, 'item' : 'winter coat', 'price_usd': 499.99, qty: 200 }",
+            "{ '_id' : 3, 'item' : 'sun dress', 'price_usd': 199.99, qty: 250 }",
+            "{ '_id' : 4, 'item' : 'leather boots', 'price_usd': 249.99, qty: 300 }",
+            "{ '_id' : 5, 'item' : 'bow tie', 'price_usd': 9.99, qty: 180 }"));
 
         List<Document> actual = getDs().aggregate("inventory")
-                                       .match(expr(gt(getField("price.usd"), value(200))))
+                                       .match(expr(gt(getField("price_usd"), value(200))))
                                        .execute(Document.class)
                                        .toList();
 
         List<Document> expected = parseDocs(
-            "{ _id: 2, item: 'winter coat', qty: 200, 'price.usd': 499.99 }",
-            "{ _id: 4, item: 'leather boots', qty: 300, 'price.usd': 249.99 }");
+            "{ _id: 2, item: 'winter coat', qty: 200, 'price_usd': 499.99 }",
+            "{ _id: 4, item: 'leather boots', qty: 300, 'price_usd': 249.99 }");
         assertListEquals(actual, expected);
 
         insert("inventory", parseDocs(
-            "{ '_id' : 1, 'item' : 'sweatshirt', '$price': 45.99, qty: 300 }",
-            "{ '_id' : 2, 'item' : 'winter coat', '$price': 499.99, qty: 200 }",
-            "{ '_id' : 3, 'item' : 'sun dress', '$price': 199.99, qty: 250 }",
-            "{ '_id' : 4, 'item' : 'leather boots', '$price': 249.99, qty: 300 }",
-            "{ '_id' : 5, 'item' : 'bow tie', '$price': 9.99, qty: 180 }"));
+            "{ '_id' : 1, 'item' : 'sweatshirt', 'literal_price': 45.99, qty: 300 }",
+            "{ '_id' : 2, 'item' : 'winter coat', 'literal_price': 499.99, qty: 200 }",
+            "{ '_id' : 3, 'item' : 'sun dress', 'literal_price': 199.99, qty: 250 }",
+            "{ '_id' : 4, 'item' : 'leather boots', 'literal_price': 249.99, qty: 300 }",
+            "{ '_id' : 5, 'item' : 'bow tie', 'literal_price': 9.99, qty: 180 }"));
 
         actual = getDs().aggregate("inventory")
-                        .match(expr(gt(getField(literal("$price")), value(200))))
+                        .match(expr(gt(getField(literal("literal_price")), value(200))))
                         .execute(Document.class)
                         .toList();
 
         expected = parseDocs(
-            "{ _id: 2, item: 'winter coat', qty: 200, '$price': 499.99 }",
-            "{ _id: 4, item: 'leather boots', qty: 300, '$price': 249.99 }");
+            "{ _id: 2, item: 'winter coat', qty: 200, 'literal_price': 499.99 }",
+            "{ _id: 4, item: 'leather boots', qty: 300, 'literal_price': 249.99 }");
         assertListEquals(actual, expected);
 
     }
