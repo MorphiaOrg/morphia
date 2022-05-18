360d5b60b1a5ec087034cd309ded9f5ecb898ea5
evanchooly <jlee@antwerkz.com>
2022-05-18 18:50:04 -0400

    bump to release version 2.2.7

--- core/src/test/java/dev/morphia/test/mapping/experimental/InstanceCreationTest.java
+++ core/src/test/java/dev/morphia/test/mapping/experimental/InstanceCreationTest.java
@@ -3,11 +3,18 @@ package dev.morphia.test.mapping.experimental;
 import dev.morphia.annotations.Entity;
 import dev.morphia.annotations.Id;
 import dev.morphia.query.FindOptions;
+import dev.morphia.query.experimental.filters.Filters;
 import dev.morphia.test.TestBase;
 import org.bson.types.ObjectId;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotSame;
 
 public class InstanceCreationTest extends TestBase {
     @Test
@@ -18,6 +25,27 @@ public class InstanceCreationTest extends TestBase {
         final Author loaded = getDs().find(Author.class).iterator(new FindOptions()
                                                                       .limit(1)).tryNext();
         assertEquals(author, loaded);
+    }
+
+    @Test
+    public void duplicates() {
+        SaveData toSave = new SaveData("id", new double[]{1,2,3}, List.of(
+            new SaveData.ChildData(new double[]{4,5,6}, new ArrayList<>()),
+            new SaveData.ChildData(new double[]{7,8,9}, new ArrayList<>()),
+            new SaveData.ChildData(new double[]{10,11,12}, new ArrayList<>())));
+        getDs().save(toSave);
+
+        SaveData loadedData = getDs().find(SaveData.class).first();
+
+        // Make sure the child objects are not the same
+        assertNotSame(loadedData.elements.get(0), loadedData.elements.get(1));
+        assertNotSame(loadedData.elements.get(0), loadedData.elements.get(2));
+        assertNotSame(loadedData.elements.get(1), loadedData.elements.get(2));
+
+        // Make sure the child position arrays are not the same
+        assertFalse(Arrays.equals(loadedData.elements.get(0).child_position, loadedData.elements.get(1).child_position));
+        assertFalse(Arrays.equals(loadedData.elements.get(0).child_position, loadedData.elements.get(2).child_position));
+        assertFalse(Arrays.equals(loadedData.elements.get(1).child_position, loadedData.elements.get(2).child_position));
     }
 
     @Entity
@@ -61,6 +89,48 @@ public class InstanceCreationTest extends TestBase {
 
         public ObjectId getId() {
             return id;
+        }
+    }
+
+    @Entity("saved_stuff")
+    public static class SaveData {
+        @Id
+        public final String saveName;
+        public final double[] position;
+        public final List<ChildData> elements;
+
+        public SaveData(String saveName, double[] position, List<ChildData> elements) {
+            this.saveName = saveName;
+            this.position = position;
+            this.elements = elements;
+        }
+
+        @Override
+        public String toString() {
+            return "SaveData{" +
+                   "saveName='" + saveName + '\'' +
+                   ", position=" + Arrays.toString(position) +
+                   ", elements=" + elements +
+                   '}';
+        }
+
+        @Entity
+        public static class ChildData {
+            public final double[] child_position;
+            public final List<ChildData> child_elements;
+
+            public ChildData(double[] child_position, List<ChildData> child_elements) {
+                this.child_position = child_position;
+                this.child_elements = child_elements;
+            }
+
+            @Override
+            public String toString() {
+                return "\nChildData{" +
+                       "child_position=" + Arrays.toString(child_position) +
+                       ", child_elements=" + child_elements +
+                       '}';
+            }
         }
     }
 }
