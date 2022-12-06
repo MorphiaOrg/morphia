eecd6cad65783951b570c14cec612188e04cf7f3
evanchooly <jlee@antwerkz.com>
2022-12-06 00:37:36 -0500

    style fix version class

--- core/src/main/java/dev/morphia/MorphiaVersion24.java
+++ core/src/main/java/dev/morphia/MorphiaVersion24.java
@@ -5,6 +5,7 @@ import dev.morphia.annotations.internal.MorphiaInternal;
 /**
  * This class is a version marker for certain frameworks to use when loading configurations, e.g. See
  * <a href="https://github.com/MorphiaOrg/morphia/issues/1496">here</a> for details.
+ * 
  * @morphia.internal
  */
 @MorphiaInternal
