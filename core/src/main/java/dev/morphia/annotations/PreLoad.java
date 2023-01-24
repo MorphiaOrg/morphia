f0105dc929401f89ad4a81a5d290b953f4e06421
eheimburg <74330250+eheimburg@users.noreply.github.com>
2023-01-24 03:13:43 -0500

    Comment clean up
    I also added a missing use-case from @PrePersist. I still didn't mention that it can take a Document parameter -- I can't think of a use-case for it, so treating it as an implementation detail

--- core/src/main/java/dev/morphia/annotations/PreLoad.java
+++ core/src/main/java/dev/morphia/annotations/PreLoad.java
@@ -11,21 +11,23 @@ import java.lang.annotation.Target;
  * Called before the data has been loaded into the object. Here you can alter the raw BSON Document from the
  * datastore prior to object initialization.
  * <p>
- * Methods with this annotation may optionally take a parameter of type {@link org.bson.Document Document} 
+ * Methods with this annotation may optionally take a parameter of type {@link org.bson.Document Document}
  * and/or a parameter of type {@link dev.morphia.Datastore Datastore}. If both parameters are used, the
  * order is unimportant. For typical usage, the method should take a Document parameter.
  * <p>
- * The Document parameter (if used) will be the document that this object will be loaded from. You can alter 
- * the Document here and those changes will be reflected when the object is loaded. The Datastore parameter 
+ * The Document parameter (if used) will be the document that this object will be loaded from. You can alter
+ * the Document here and those changes will be reflected when the object is loaded. The Datastore parameter
  * (if used) will be the Datastore this Document was loaded from.
  * <p>
  * <strong>Example declarations (in order of most to least typical):</strong>
+ * 
  * <pre>
  * {@code @PreLoad}
  * private void fixupDocument(Document doc) {
  *   // doc is the Document we will be initialized from. Document elements can be manually changed or removed.
  * }
  * </pre>
+ * 
  * <pre>
  * {@code @PreLoad}
  * private void performAdditionalQueriesBeforeLoading(Document doc, Datastore datastore) {
--- core/src/main/java/dev/morphia/annotations/PrePersist.java
+++ core/src/main/java/dev/morphia/annotations/PrePersist.java
@@ -8,14 +8,26 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Called before the data has been persisted to the datastore (before mapping is done). 
+ * Called before the data has been persisted to the datastore (before mapping is done).
  * Here you can alter your class members prior to being saved.
  * <p>
- * <strong>Example declaration:</strong>
+ * Methods with this annotation may optionally take a parameter of type
+ * {@link dev.morphia.Datastore Datastore}. This is the Datastore that
+ * data will be persisted to.
+ * <p>
+ * <strong>Example declarations (in order of most to least typical):</strong>
+ * 
  * <pre>
  * {@code @PrePersist}
  * void cleanUpBeforeSaving() {
  *   // perform variable sanitization here as needed
+ * }
+ * </pre>
+ * 
+ * <pre>
+ * {@code @PrePersist}
+ * void retrieveOtherInfoBeforeSaving(Datastore datastore) {
+ *   // query datastore as needed
  * }
  * </pre>
  * 
