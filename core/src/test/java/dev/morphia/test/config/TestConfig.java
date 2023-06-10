34c45eed9ab24c7ea83d259905d89358035c941c
evanchooly <jlee@antwerkz.com>
2023-06-09 22:32:20 -0400

    testing include file

--- core/src/test/java/dev/morphia/test/config/TestConfig.java
+++ core/src/test/java/dev/morphia/test/config/TestConfig.java
@@ -8,6 +8,10 @@ import dev.morphia.test.TestBase;
 
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TestConfig extends TestBase {
     @Test
     public void testConfig() {
@@ -15,8 +19,28 @@ public class TestConfig extends TestBase {
     }
 
     @Test
-    public void testConfigWithMapperOptions() {
-        Datastore datastore = Morphia.createDatastore(getMongoClient(), "dummy", MapperOptions.DEFAULT);
+    public void testConfigWithMapperOptions() throws IOException {
+        var root = findProjectRoot();
+
+        var docsTarget = new File(root, "docs/modules/ROOT/pages");
+
+        try (var writer = new FileWriter(new File(docsTarget, "complete-morphia-config.properties"))) {
+            var configContents = MorphiaConfigHelper.dumpConfigurationFile(MapperOptions.DEFAULT, "dummy", true);
+            writer.write(configContents);
+            writer.flush();
+        }
+    }
+
+    private File findProjectRoot() {
+        var root = new File(".").getAbsoluteFile();
+        while (root != null && !new File(root, ".git").exists()) {
+            root = root.getParentFile();
+        }
+
+        if (root == null) {
+            throw new IllegalStateException("Could not find .git directory");
+        }
+        return root;
     }
 
     @Test
