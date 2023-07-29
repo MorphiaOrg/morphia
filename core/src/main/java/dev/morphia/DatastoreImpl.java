b5d93810319d6b7d6d92994ae98c968ad5bb058a
evanchooly <jlee@antwerkz.com>
2023-07-29 17:45:54 -0400

    Forward port index error handling
    #2453

--- core/src/main/java/dev/morphia/DatastoreImpl.java
+++ core/src/main/java/dev/morphia/DatastoreImpl.java
@@ -719,7 +719,7 @@ public class DatastoreImpl implements AdvancedDatastore {
             } else {
                 ReplaceOptions updateOptions = new ReplaceOptions()
                         .bypassDocumentValidation(options.bypassDocumentValidation())
-                        .upsert(true);
+                        .upsert(!info.versioned);
                 Document filter = new Document("_id", id);
                 info.filter(filter);
                 entityModel.getShardKeys().forEach((property) -> {
@@ -736,7 +736,6 @@ public class DatastoreImpl implements AdvancedDatastore {
         } catch (MongoWriteException e) {
             if (info.versioned()) {
                 info.rollbackVersion();
-                throw new VersionMismatchException(entity.getClass(), id);
             }
             throw e;
         }
