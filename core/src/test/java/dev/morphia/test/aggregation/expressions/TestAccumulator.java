50336f3634f83aceeb4c0c853e9ff13378c7e1c5
evanchooly <jlee@antwerkz.com>
2023-12-08 13:59:25 -0500

    style updates

--- core/src/test/java/dev/morphia/test/aggregation/expressions/TestAccumulator.java
+++ core/src/test/java/dev/morphia/test/aggregation/expressions/TestAccumulator.java
@@ -43,38 +43,38 @@ public class TestAccumulator extends AggregationTest {
                                           return (state.sum / state.count)
                                         }"""))));
     }
+
     @Test
     public void testExample2() {
         skipPipelineCheck = true;
         testPipeline(ANY, false, false, aggregation -> aggregation
                 .group(Group.group(Group.id().field("city", field("city")))
-                           .field("restaurants", accumulator(
-                               """
-                                   function(city, userProfileCity) {       \s
-                                             return {
-                                               max: city === userProfileCity ? 3 : 1,    \s
-                                               restaurants: []                           \s
-                                             }\s
-                                           }""",
-                               """
-                                   function(state, restaurantName) { \s
-                                             if (state.restaurants.length < state.max) {
-                                               state.restaurants.push(restaurantName);
-                                             }
-                                             return state;
-                                           }""",
-                               List.of(field("name")), """
-                                   function(state1, state2) {             \s
-                                             return {
-                                               max: state1.max,
-                                               restaurants: state1.restaurants.concat(state2.restaurants).slice(0, state1.max)
-                                             }\s
-                                           }""")
-                                                     .initArgs(List.of(field("city"), value("Bettles")))
-                                                     .finalizeFunction("""
-                                                         function(state) {
-                                                                   return state.restaurants
-                                                                 }"""))
-                       ));
+                        .field("restaurants", accumulator(
+                                """
+                                        function(city, userProfileCity) {       \s
+                                                  return {
+                                                    max: city === userProfileCity ? 3 : 1,    \s
+                                                    restaurants: []                           \s
+                                                  }\s
+                                                }""",
+                                """
+                                        function(state, restaurantName) { \s
+                                                  if (state.restaurants.length < state.max) {
+                                                    state.restaurants.push(restaurantName);
+                                                  }
+                                                  return state;
+                                                }""",
+                                List.of(field("name")), """
+                                        function(state1, state2) {             \s
+                                                  return {
+                                                    max: state1.max,
+                                                    restaurants: state1.restaurants.concat(state2.restaurants).slice(0, state1.max)
+                                                  }\s
+                                                }""")
+                                .initArgs(List.of(field("city"), value("Bettles")))
+                                .finalizeFunction("""
+                                        function(state) {
+                                                  return state.restaurants
+                                                }"""))));
     }
 }
