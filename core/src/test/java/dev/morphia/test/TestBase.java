382b0e914fd4ac0958972b00101101879a4ceeec
evanchooly <jlee@antwerkz.com>
2023-12-08 13:58:59 -0500

    style updates

--- core/src/test/java/dev/morphia/test/TestBase.java
+++ core/src/test/java/dev/morphia/test/TestBase.java
@@ -39,7 +39,6 @@ import org.testng.annotations.BeforeMethod;
 
 import static dev.morphia.internal.MorphiaInternals.proxyClassesPresent;
 import static java.lang.String.format;
-import static java.lang.String.join;
 import static java.nio.file.Files.lines;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -159,11 +158,11 @@ public abstract class TestBase extends MorphiaTestSetup {
         expected.forEach(
                 d -> assertTrueLazy(actual.contains(coerceToLong(d)), () -> {
                     String actualString = actual.stream()
-                                           .map(c -> c.toString())
-                                           .collect(joining("\n\t", "actual list:\n", ""));
+                            .map(c -> c.toString())
+                            .collect(joining("\n\t", "actual list:\n", ""));
                     String expectedString = expected.stream()
-                                           .map(c -> c.toString())
-                                           .collect(joining("\n\t", "expected list:\n", ""));
+                            .map(c -> c.toString())
+                            .collect(joining("\n\t", "expected list:\n", ""));
                     return format("Lists do not match:\n%s \n%s", actualString, expectedString);
                 }));
     }
