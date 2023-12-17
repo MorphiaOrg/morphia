06e8076fecab5b0343cb800a5794824722d95221
evanchooly <jlee@antwerkz.com>
2023-12-16 19:32:14 -0500

    update tests for the comparison expressions

---
+++ core/src/test/java/dev/morphia/test/aggregation/expressions/TestGte.java
@@ -0,0 +1,24 @@
+package dev.morphia.test.aggregation.expressions;
+
+import dev.morphia.test.ServerVersion;
+import dev.morphia.test.aggregation.AggregationTest;
+
+import org.testng.annotations.Test;
+
+import static dev.morphia.aggregation.expressions.ComparisonExpressions.gte;
+import static dev.morphia.aggregation.expressions.Expressions.field;
+import static dev.morphia.aggregation.expressions.Expressions.value;
+import static dev.morphia.aggregation.stages.Projection.project;
+
+public class TestGte extends AggregationTest {
+    @Test
+    public void testExample1() {
+        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+                project()
+                        .suppressId()
+                        .include("item")
+                        .include("qty")
+                        .include("qtyGte250", gte(field("qty"), value(250)))));
+    }
+
+}
---
+++ core/src/test/java/dev/morphia/test/aggregation/expressions/TestLte.java
@@ -0,0 +1,24 @@
+package dev.morphia.test.aggregation.expressions;
+
+import dev.morphia.test.ServerVersion;
+import dev.morphia.test.aggregation.AggregationTest;
+
+import org.testng.annotations.Test;
+
+import static dev.morphia.aggregation.expressions.ComparisonExpressions.lte;
+import static dev.morphia.aggregation.expressions.Expressions.field;
+import static dev.morphia.aggregation.expressions.Expressions.value;
+import static dev.morphia.aggregation.stages.Projection.project;
+
+public class TestLte extends AggregationTest {
+    @Test
+    public void testExample1() {
+        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+                project()
+                        .suppressId()
+                        .include("item")
+                        .include("qty")
+                        .include("qtyLte250", lte(field("qty"), value(250)))));
+    }
+
+}
