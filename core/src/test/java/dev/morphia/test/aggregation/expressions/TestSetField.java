64f0002e24550e57e835822fc883732fa93a1929
evanchooly <jlee@antwerkz.com>
2023-12-18 22:37:52 -0500

    add version filter

--- core/src/test/java/dev/morphia/test/aggregation/expressions/TestSetField.java
+++ core/src/test/java/dev/morphia/test/aggregation/expressions/TestSetField.java
@@ -1,6 +1,5 @@
 package dev.morphia.test.aggregation.expressions;
 
-import dev.morphia.test.ServerVersion;
 import dev.morphia.test.aggregation.AggregationTest;
 
 import org.testng.annotations.Test;
@@ -14,45 +13,46 @@ import static dev.morphia.aggregation.stages.Match.match;
 import static dev.morphia.aggregation.stages.ReplaceWith.replaceWith;
 import static dev.morphia.aggregation.stages.Unset.unset;
 import static dev.morphia.query.filters.Filters.eq;
+import static dev.morphia.test.ServerVersion.*;
 
 public class TestSetField extends AggregationTest {
     @Test
     public void testExample1() {
-        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+        testPipeline(v50, false, true, (aggregation) -> aggregation.pipeline(
                 replaceWith(setField("price.usd", ROOT, field("price"))),
                 unset("price")));
     }
 
     @Test
     public void testExample2() {
-        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+        testPipeline(v50, false, true, (aggregation) -> aggregation.pipeline(
                 replaceWith(setField(literal("$price"), ROOT, field("price"))),
                 unset("price")));
     }
 
     @Test
     public void testExample3() {
-        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+        testPipeline(v50, false, true, (aggregation) -> aggregation.pipeline(
                 match(eq("_id", 1)),
                 replaceWith(setField("price.usd", ROOT, value(49.99)))));
     }
 
     @Test
     public void testExample4() {
-        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+        testPipeline(v50, false, true, (aggregation) -> aggregation.pipeline(
                 match(eq("_id", 1)),
                 replaceWith(setField(literal("$price"), ROOT, value(49.99)))));
     }
 
     @Test
     public void testExample5() {
-        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+        testPipeline(v50, false, true, (aggregation) -> aggregation.pipeline(
                 replaceWith(setField("price.usd", ROOT, REMOVE))));
     }
 
     @Test
     public void testExample6() {
-        testPipeline(ServerVersion.ANY, false, true, (aggregation) -> aggregation.pipeline(
+        testPipeline(v50, false, true, (aggregation) -> aggregation.pipeline(
                 replaceWith(setField(literal("$price"), ROOT, REMOVE))));
     }
 
