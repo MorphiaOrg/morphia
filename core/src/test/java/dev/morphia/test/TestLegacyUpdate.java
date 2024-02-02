820f5a6c1370b4bdd3998db24ee3c5c62dda4742
Doug Jones <djones@squarespace.com>
2024-02-02 16:56:21 -0500

    Add test coverage for the rest of the legacy update api.

--- core/src/test/java/dev/morphia/test/TestLegacyUpdate.java
+++ core/src/test/java/dev/morphia/test/TestLegacyUpdate.java
@@ -8,8 +8,10 @@ import java.util.Set;
 
 import com.mongodb.client.result.UpdateResult;
 
+import dev.morphia.UpdateOptions;
 import dev.morphia.annotations.Entity;
 import dev.morphia.annotations.Id;
+import dev.morphia.query.PushOptions;
 import dev.morphia.query.Query;
 import dev.morphia.query.UpdateOperations;
 import dev.morphia.query.filters.Filter;
@@ -42,7 +44,170 @@ public class TestLegacyUpdate extends TestBase {
     }
 
     @Test
-    public void testRemoveAllUpdate() {
+    public void testAddToSet() {
+        createTestDocuments();
+
+        ObjectId toAdd = ObjectId.get();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .addToSet("idSet", toAdd);
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument updated = findMyDocument();
+        Assert.assertTrue(updated.idSet.contains(toAdd));
+    }
+
+    @Test
+    public void testAddListToSet() {
+        createTestDocuments();
+
+        List<ObjectId> toAdd = List.of(ObjectId.get(), ObjectId.get());
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .addToSet("idSet", toAdd);
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument updated = findMyDocument();
+        for (ObjectId added : toAdd) {
+            Assert.assertTrue(updated.idSet.contains(added));
+        }
+    }
+
+    @Test
+    public void testDec() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .dec("intField");
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.intField, 0);
+    }
+
+    @Test
+    public void testDecNumber() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .dec("intField", new Long(2));
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.intField, -1);
+    }
+
+    @Test
+    public void testInc() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .inc("intField");
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.intField, 2);
+    }
+
+    @Test
+    public void testMax() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .max("intField", 9);
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.intField, 9);
+    }
+
+    @Test
+    public void testMin() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .min("intField", -1);
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.intField, -1);
+    }
+
+    @Test
+    public void testPush() {
+        createTestDocuments();
+        ObjectId toAdd = ObjectId.get();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .push("idSet", toAdd);
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertTrue(resultDoc.idSet.contains(toAdd));
+    }
+
+    @Test
+    public void testPushWithOptions() {
+        createTestDocuments();
+
+        ObjectId toAdd = ObjectId.get();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class).filter("embeddedDocs.field1 in",
+            List.of("foo1"));
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .push("embeddedDocs.$.objectIds", toAdd, PushOptions.options().position(1));
+
+        UpdateResult result = getDs().update(query, update, new UpdateOptions().multi(true));
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.embeddedDocs.get(0).objectIds.get(1), toAdd);
+    }
+
+    @Test
+    public void testPushList() {
+        createTestDocuments();
+        List<ObjectId> toAdd = List.of(ObjectId.get(), ObjectId.get());
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .push("idSet", toAdd);
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        for (ObjectId added : toAdd) {
+            Assert.assertTrue(resultDoc.idSet.contains(added));
+        }
+    }
+
+    @Test
+    public void testRemoveAll() {
         createTestDocuments();
 
         Query<MyDocument> query = getDs().createQuery(MyDocument.class);
@@ -54,9 +219,24 @@ public class TestLegacyUpdate extends TestBase {
         UpdateResult result = getDs().update(query, removeAllOp);
         Assert.assertEquals(result.getModifiedCount(), 1);
 
-        Query<MyDocument> updatedQuery = getDs().find(MyDocument.class);
-        MyDocument resultDoc = updatedQuery.first();
+        MyDocument resultDoc = findMyDocument();
         Assert.assertFalse(resultDoc.embeddedDocs.contains(testEmbeddedDoc1));
+    }
+
+    @Test
+    public void testRemoveAllList() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class).filter("embeddedDocs.field1", testEmbeddedDoc2.field1);
+
+        UpdateOperations<MyDocument> removeAllOp = getDs().createUpdateOperations(MyDocument.class)
+            .removeAll("embeddedDocs.$.objectIds", testObjectIds);
+
+        UpdateResult result = getDs().update(query, removeAllOp);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertTrue(resultDoc.embeddedDocs.get(1).objectIds.isEmpty());
     }
 
     @Test
@@ -98,6 +278,81 @@ public class TestLegacyUpdate extends TestBase {
     }
 
     @Test
+    public void testRemoveFirst() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .removeFirst("embeddedDocs");
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertFalse(resultDoc.embeddedDocs.contains(testEmbeddedDoc1));
+    }
+
+    @Test
+    public void testRemoveLast() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .removeLast("embeddedDocs");
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertFalse(resultDoc.embeddedDocs.contains(testEmbeddedDoc2));
+    }
+
+    @Test
+    public void testSet() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .set("intField", 100);
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.intField, 100);
+    }
+
+    @Test
+    public void testSetOnInsert() {
+        ObjectId id = ObjectId.get();
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class).filter("id = ", id);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .setOnInsert("intField", 999);
+
+        UpdateResult result = getDs().update(query, update, new UpdateOptions().upsert(true));
+        Assert.assertEquals(result.getModifiedCount(), 0);
+        Assert.assertEquals(result.getUpsertedId().asObjectId().getValue(), id);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertEquals(resultDoc.intField, 999);
+    }
+
+    @Test
+    public void testUnset() {
+        createTestDocuments();
+
+        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
+        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
+            .unset("intField");
+
+        UpdateResult result = getDs().update(query, update);
+        Assert.assertEquals(result.getModifiedCount(), 1);
+
+        MyDocument resultDoc = findMyDocument();
+        Assert.assertNull(resultDoc.intField);
+    }
+
+    @Test
     public void testUpdateToString() {
         UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
                 .removeAll("embeddedDocs", testEmbeddedDoc1);
@@ -106,20 +361,14 @@ public class TestLegacyUpdate extends TestBase {
         Assert.assertTrue(test != null && !test.isEmpty());
     }
 
-    @Test
-    public void testAddToSet() {
-        createTestDocuments();
-
-        Query<MyDocument> query = getDs().createQuery(MyDocument.class);
-        UpdateOperations<MyDocument> update = getDs().createUpdateOperations(MyDocument.class)
-                .addToSet("idSet", ObjectId.get());
-
-        UpdateResult result = getDs().update(query, update);
-        Assert.assertEquals(result.getModifiedCount(), 1);
+    private MyDocument findMyDocument() {
+        Query<MyDocument> updatedQuery = getDs().find(MyDocument.class);
+        return updatedQuery.first();
     }
 
     private List<ObjectId> createTestDocuments() {
         MyDocument myDocument = new MyDocument();
+        myDocument.intField = 1;
         myDocument.embeddedDocs.addAll(List.of(testEmbeddedDoc1, testEmbeddedDoc2));
         myDocument = getDs().save(myDocument);
         return List.of(myDocument.id);
@@ -134,6 +383,8 @@ public class TestLegacyUpdate extends TestBase {
         public List<EmbeddedDocument> embeddedDocs = new ArrayList<>();
 
         public Set<ObjectId> idSet = new HashSet<>();
+
+        public Integer intField;
     }
 
     @Entity
