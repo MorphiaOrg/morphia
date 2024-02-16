1a81db45e065a6dd95fe63f725d2788d900bfc1b
evanchooly <jlee@antwerkz.com>
2024-02-15 23:10:16 -0500

    updating more tests

--- core/src/test/java/dev/morphia/test/aggregation/expressions/TestMeta.java
+++ core/src/test/java/dev/morphia/test/aggregation/expressions/TestMeta.java
@@ -3,7 +3,6 @@ package dev.morphia.test.aggregation.expressions;
 import dev.morphia.test.ServerVersion;
 import dev.morphia.test.aggregation.AggregationTest;
 
-import org.bson.Document;
 import org.testng.annotations.Test;
 
 import static dev.morphia.aggregation.expressions.AccumulatorExpressions.sum;
@@ -14,13 +13,12 @@ import static dev.morphia.aggregation.stages.Group.group;
 import static dev.morphia.aggregation.stages.Group.id;
 import static dev.morphia.aggregation.stages.Match.match;
 import static dev.morphia.query.filters.Filters.eq;
+import static dev.morphia.query.filters.Filters.gte;
 import static dev.morphia.query.filters.Filters.text;
 
 public class TestMeta extends AggregationTest {
     @Test
     public void testExample1() {
-        getDatabase().getCollection(AGG_TEST_COLLECTION)
-                .createIndex(new Document("title", "text"));
         testPipeline(ServerVersion.ANY, false, false, (aggregation) -> aggregation.pipeline(
                 match(text("cake")),
                 group(id(meta()))
@@ -29,11 +27,16 @@ public class TestMeta extends AggregationTest {
 
     @Test
     public void testExample2() {
-        getDatabase().getCollection(AGG_TEST_COLLECTION)
-                .createIndex(new Document("type", 1).append("item", 1));
-
         testPipeline(ServerVersion.ANY, true, true, (aggregation) -> aggregation.pipeline(
                 match(eq("type", "apparel")),
+                addFields()
+                        .field("idxKey", meta(INDEXKEY))));
+    }
+
+    @Test
+    public void testExample3() {
+        testPipeline(ServerVersion.ANY, true, true, (aggregation) -> aggregation.pipeline(
+                match(gte("price", 10.0)),
                 addFields()
                         .field("idxKey", meta(INDEXKEY))));
     }
