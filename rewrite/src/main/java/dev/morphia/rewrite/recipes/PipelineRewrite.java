5de526885fadd49e11a394cbee5282c0805490fe
evanchooly <jlee@antwerkz.com>
2024-05-12 20:54:13 -0400

    remove debug

--- rewrite/src/main/java/dev/morphia/rewrite/recipes/PipelineRewrite.java
+++ rewrite/src/main/java/dev/morphia/rewrite/recipes/PipelineRewrite.java
@@ -8,11 +8,7 @@ import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.MethodMatcher;
-import org.openrewrite.java.tree.Expression;
-import org.openrewrite.java.tree.J.Identifier;
 import org.openrewrite.java.tree.J.MethodInvocation;
-import org.openrewrite.java.tree.JavaType;
-import org.openrewrite.java.tree.JavaType.Class;
 
 public class PipelineRewrite extends Recipe {
 
@@ -68,23 +64,8 @@ public class PipelineRewrite extends Recipe {
         return new JavaIsoVisitor<>() {
 
             @Override
-            public Identifier visitIdentifier(Identifier identifier, ExecutionContext executionContext) {
-                System.out.println("identifier.getSimpleName() = " + identifier.getSimpleName());
-                JavaType type = identifier.getType();
-                System.out.println("identifier.getType() = " + type);
-                if (type instanceof Class klass && klass.getFullyQualifiedName().equals("dev.morphia.aggregation.Aggregation")) {
-                    System.out.println("klass = " + klass);
-                }
-                return super.visitIdentifier(identifier, executionContext);
-            }
-
-            @Override
             public MethodInvocation visitMethodInvocation(MethodInvocation methodInvocation, @NotNull ExecutionContext context) {
                 if (matchers.stream().anyMatch(matcher -> matcher.matches(methodInvocation))) {
-                    Expression expression = methodInvocation.getArguments().get(0);
-                    System.out.println("method = " + methodInvocation.getMethodType());
-                    //                    System.out.println("expression = " + expression);
-
                     return super.visitMethodInvocation(methodInvocation
                             .withName(methodInvocation.getName().withSimpleName("pipeline")),
                             context);
