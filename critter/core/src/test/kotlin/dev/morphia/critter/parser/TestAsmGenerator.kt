a57afb798760262ee12e35e524a1a0fb7ea47b65
evanchooly <jlee@antwerkz.com>
2024-06-30 21:08:59 -0400

    remove debug

--- critter/core/src/test/kotlin/dev/morphia/critter/parser/TestAsmGenerator.kt
+++ critter/core/src/test/kotlin/dev/morphia/critter/parser/TestAsmGenerator.kt
@@ -24,8 +24,6 @@ class TestAsmGenerator {
             .getConstructor()
             .newInstance()
 
-        println("**************** entity::class.java.methods = ${entity::class.java.methods.joinToString("\n")}")
-
         val generator = EntityAccessorGenerator(DummyEntity::class.java, "name", String::class.java)
         critterClassLoader.register(generator.accessorType.className, generator.dump())
 
