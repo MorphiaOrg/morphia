62a9c4de6726bb5f84b7f40ab302c68b6de1ba19
evanchooly <jlee@antwerkz.com>
2024-08-18 23:29:17 -0400

    remove duplicate class name

--- core/src/test/java/dev/morphia/test/TestDiscriminatorLookup.java
+++ core/src/test/java/dev/morphia/test/TestDiscriminatorLookup.java
@@ -1,6 +1,8 @@
 package dev.morphia.test;
 
 import dev.morphia.annotations.Entity;
+import dev.morphia.test.models.Shape;
+import dev.morphia.test.models.Square;
 import dev.morphia.test.models.TestEntity;
 
 import org.testng.annotations.Test;
@@ -14,14 +16,14 @@ public class TestDiscriminatorLookup extends TestBase {
     public void testLookup() {
         withConfig(buildConfig(SomeEntity.class), () -> {
             final SomeEntity entity = new SomeEntity();
-            entity.setShape(new Shape.Square());
+            entity.setShape(new Square());
 
             getDs().save(entity);
         });
 
         final SomeEntity entity = getDs().find(SomeEntity.class).first();
         assertNotNull(entity);
-        assertTrue(Shape.Square.class.isInstance(entity.getShape()));
+        assertTrue(Square.class.isInstance(entity.getShape()));
     }
 
     @Entity
@@ -34,13 +36,6 @@ public class TestDiscriminatorLookup extends TestBase {
 
         public void setShape(Shape shape) {
             this.shape = shape;
-        }
-    }
-
-    @Entity
-    public static abstract class Shape {
-        public static class Square extends Shape {
-            public double side;
         }
     }
 }
