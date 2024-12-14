321bf8567f8deb4df2b3e4d5a374b50661e6321c
evanchooly <jlee@antwerkz.com>
2024-12-14 18:36:52 -0500

    CreateDatastoreMigration refined

--- rewrite/src/main/java/dev/morphia/rewrite/recipes/CreateDatastoreMigration.java
+++ rewrite/src/main/java/dev/morphia/rewrite/recipes/CreateDatastoreMigration.java
@@ -1,5 +1,9 @@
 package dev.morphia.rewrite.recipes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.jetbrains.annotations.NotNull;
 import org.openrewrite.Cursor;
 import org.openrewrite.ExecutionContext;
@@ -7,6 +11,7 @@ import org.openrewrite.Preconditions;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
 import org.openrewrite.java.JavaIsoVisitor;
+import org.openrewrite.java.JavaParser;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.UsesType;
@@ -14,11 +19,6 @@ import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J.Identifier;
 import org.openrewrite.java.tree.J.MethodInvocation;
 import org.openrewrite.java.tree.J.TypeCast;
-import org.openrewrite.java.tree.JavaType.Method;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static java.util.List.of;
 import static org.openrewrite.java.tree.JavaType.buildType;
@@ -41,55 +41,63 @@ public class CreateDatastoreMigration extends Recipe {
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
         return Preconditions.check(new UsesType<>(OLD_TYPE, true),
-            new MorphiaConfigMigrationVisitor());
+                new MorphiaConfigMigrationVisitor());
     }
 
     private static class MorphiaConfigMigrationVisitor extends JavaIsoVisitor<ExecutionContext> {
-        private static final MethodMatcher METHOD_MATCHER = new MethodMatcher("dev.morphia.Morphia createDatastore(..)");
+        private static final MethodMatcher METHOD_MATCHER = new MethodMatcher(
+                "dev.morphia.Morphia createDatastore(com.mongodb.client.MongoClient, String, dev.morphia.mapping" +
+                        ".MapperOptions)");
 
         private final JavaTemplate databaseCall = JavaTemplate.builder("MorphiaConfig.load().database(#{})")
-                                                              .build();
+                                .javaParser(JavaParser.fromJavaVersion()
+                                                      .classpath("morphia-core"))
+                .imports(NEW_TYPE)
+                .build();
 
         @Override
         public MethodInvocation visitMethodInvocation(@NotNull MethodInvocation methodInvocation, @NotNull ExecutionContext context) {
             if (METHOD_MATCHER.matches(methodInvocation)) {
-                maybeAddImport(NEW_TYPE);
+                maybeAddImport(NEW_TYPE, null, false);
                 maybeRemoveImport(OLD_TYPE);
 
                 List<Expression> arguments = methodInvocation.getArguments();
-                if (arguments.size() != 3) {
-                    return methodInvocation;
-                }
                 var databaseName = arguments.get(1);
-                MethodInvocation load = findLoad(arguments.get(2));
-                Cursor scope = new Cursor(getCursor(), load);
-                MethodInvocation applied = databaseCall.apply(scope, load.getCoordinates().replace(), databaseName);
+                Expression builder = arguments.get(2);
+                Cursor scope = new Cursor(getCursor(), builder);
+                MethodInvocation applied = databaseCall.apply(scope, builder.getCoordinates().replace(), databaseName);
 
-                TypeCast typeCast = (TypeCast) arguments.get(2);
-                var expressions = flatten(typeCast);
-                Method methodType = ((MethodInvocation) typeCast.getExpression()).getMethodType()
-                                                                                 .withName("database")
-                                                                                 .withParameterTypes(
-                                                                                     of(buildType(String.class.getName())));
-                expressions.add(2, applied.withMethodType(methodType));
+                var expressions = flatten(builder);
+
+/*
+                applied = applied.withMethodType(((MethodInvocation) builder).getMethodType()
+                        .withName("database")
+                        .withParameterTypes(
+                                of(buildType(String.class.getName()))));
+*/
+
+                expressions.set(0, applied);
+                expressions.remove(1);
+                expressions.removeIf(
+                        expression -> expression instanceof MethodInvocation invocation && invocation.getSimpleName().equals("build"));
                 var argument = expressions.subList(1, expressions.size()).stream().reduce(expressions.get(0),
-                    (current, next) -> ((MethodInvocation) next).withSelect(current));
+                        (current, next) -> ((MethodInvocation) next).withSelect(current));
 
                 MethodInvocation after = methodInvocation
-                                             .withMethodType(methodInvocation
-                                                                 .getMethodType()
-                                                                 .withParameterTypes(of(buildType(String.class.getName()),
-                                                                     buildType(NEW_TYPE))))
-                                             .withArguments(of(arguments.get(0), argument));
+                        .withMethodType(methodInvocation
+                                .getMethodType()
+                                .withParameterTypes(of(buildType(String.class.getName()),
+                                        buildType(NEW_TYPE))))
+                        .withArguments(of(arguments.get(0), argument));
                 return maybeAutoFormat(methodInvocation, after, context);
             } else {
                 return super.visitMethodInvocation(methodInvocation, context);
             }
         }
 
-        private static @NotNull ArrayList<Expression> flatten(TypeCast typeCast) {
+        private static @NotNull ArrayList<Expression> flatten(Expression start) {
             var expressions = new ArrayList<Expression>();
-            var expression = typeCast.getExpression();
+            var expression = start;
             while (expression != null) {
                 if (expression instanceof MethodInvocation invocation) {
                     expressions.add(invocation);
--- rewrite/src/test/java/dev/morphia/rewrite/recipes/test/CreateDatastoreMigrationTest.java
+++ rewrite/src/test/java/dev/morphia/rewrite/recipes/test/CreateDatastoreMigrationTest.java
@@ -1,7 +1,7 @@
 package dev.morphia.rewrite.recipes.test;
 
 import dev.morphia.rewrite.recipes.CreateDatastoreMigration;
-import dev.morphia.rewrite.recipes.MorphiaConfigMigration;
+
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.openrewrite.Recipe;
@@ -18,38 +18,39 @@ public class CreateDatastoreMigrationTest extends MorphiaRewriteTest {
     @Test
     public void update() {
         rewriteRun(
-          //language=java
-          java(
-            """
-              import com.mongodb.client.MongoClient;
-              import dev.morphia.Datastore;
-              import dev.morphia.Morphia;
-              import dev.morphia.config.MorphiaConfig;import dev.morphia.mapping.MapperOptions;
-              
-              public class UnwrapTest {
-                  public void update() {
-                      MongoClient client = null;
-                      Datastore datastore = Morphia.createDatastore(client, "benchmarks", (MapperOptions) MorphiaConfig.load()
-                                                                                                       .applyIndexes(false)
-                                                                                                       .applyCaps(true));
-                  }
-              }
-              """,
-            """
-                import com.mongodb.client.MongoClient;
-                import dev.morphia.Datastore;
-                import dev.morphia.Morphia;
-                import dev.morphia.config.MorphiaConfig;
-                
-                public class UnwrapTest {
-                    public void update() {
-                        MongoClient client = null;
-                        Datastore datastore = Morphia.createDatastore(client, MorphiaConfig.load().database("benchmarks")
-                                .applyIndexes(false)
-                                .applyCaps(true));
-                    }
-                }
-                """));
+                //language=java
+                java(
+                        """
+                                import com.mongodb.client.MongoClient;
+                                import dev.morphia.Datastore;
+                                import dev.morphia.Morphia;
+                                import dev.morphia.mapping.MapperOptions;
+
+                                public class UnwrapTest {
+                                    public void update() {
+                                        MongoClient client = null;
+                                        Datastore datastore = Morphia.createDatastore(client, "benchmarks", MapperOptions.builder()
+                                                                                                                         .discriminatorKey("__type")
+                                                                                                                         .mapSubPackages(true)
+                                                                                                                         .build());
+                                    }
+                                }
+                                """,
+                        """
+                                import com.mongodb.client.MongoClient;
+                                import dev.morphia.Datastore;
+                                import dev.morphia.Morphia;
+                                import dev.morphia.config.MorphiaConfig;
+
+                                public class UnwrapTest {
+                                    public void update() {
+                                        MongoClient client = null;
+                                        Datastore datastore = Morphia.createDatastore(client,  MorphiaConfig.load().database("benchmarks")
+                                                .discriminatorKey("__type")
+                                                .mapSubPackages(true));
+                                    }
+                                }
+                                """));
 
     }
 
