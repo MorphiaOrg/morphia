c4f97ccaa388bf2c291683b1da4193e5fb33d17d
evanchooly <jlee@antwerkz.com>
2025-01-05 18:16:10 -0500

    fix pipeline formatting add some internal recipes for upgrade testing

--- rewrite/src/main/java/dev/morphia/rewrite/recipes/PipelineRewrite.java
+++ rewrite/src/main/java/dev/morphia/rewrite/recipes/PipelineRewrite.java
@@ -32,62 +32,62 @@ public class PipelineRewrite extends Recipe {
     public static final MethodMatcher PIPELINE = new MethodMatcher(AGGREGATION + " pipeline(..)");
 
     static final List<MethodMatcher> matchers = List.of(
-        new MethodMatcher(AGGREGATION + " addFields(dev.morphia.aggregation.stages.AddFields)"),
-        new MethodMatcher(AGGREGATION + " autoBucket(dev.morphia.aggregation.stages.AutoBucket)"),
-        new MethodMatcher(AGGREGATION + " bucket(dev.morphia.aggregation.stages.Bucket)"),
-        new MethodMatcher(AGGREGATION + " changeStream()"),
-        new MethodMatcher(AGGREGATION + " changeStream(dev.morphia.aggregation.stages.ChangeStream)"),
-        new MethodMatcher(AGGREGATION + " collStats(dev.morphia.aggregation.stages.CollectionStats)"),
-        new MethodMatcher(AGGREGATION + " count(dev.morphia.aggregation.stages.Count)"),
-        new MethodMatcher(AGGREGATION + " currentOp(dev.morphia.aggregation.stages.CountOp)"),
-        new MethodMatcher(AGGREGATION + " densify(dev.morphia.aggregation.stages.Densify)"),
-        new MethodMatcher(AGGREGATION + " documents(dev.morphia.aggregation.expressions.impls.DocumentExpression)"),
-        new MethodMatcher(AGGREGATION + " facet(dev.morphia.aggregation.stages.Facet)"),
-        new MethodMatcher(AGGREGATION + " fill(dev.morphia.aggregation.stages.Fill)"),
-        new MethodMatcher(AGGREGATION + " geoNear(dev.morphia.aggregation.stages.GeoNear)"),
-        new MethodMatcher(AGGREGATION + " graphLookup(dev.morphia.aggregation.stages.GraphLookup)"),
-        new MethodMatcher(AGGREGATION + " group(dev.morphia.aggregation.stages.Group)"),
-        new MethodMatcher(AGGREGATION + " indexStats(dev.morphia.aggregation.stages.IndexStats)"),
-        new MethodMatcher(AGGREGATION + " limit(long)"),
-        new MethodMatcher(AGGREGATION + " lookup(dev.morphia.aggregation.stages.Lookup)"),
-        new MethodMatcher(AGGREGATION + " match(dev.morphia.query.filters.Filter...)"),
-        new MethodMatcher(AGGREGATION + " planCacheStats()"),
-        new MethodMatcher(AGGREGATION + " project(dev.morphia.aggregation.stages.Projection)"),
-        new MethodMatcher(AGGREGATION + " redact(dev.morphia.aggregation.stages.Redact)"),
-        new MethodMatcher(AGGREGATION + " replaceRoot(dev.morphia.aggregation.stages.ReplaceRoot)"),
-        new MethodMatcher(AGGREGATION + " replaceWith(dev.morphia.aggregation.stages.ReplaceWith)"),
-        new MethodMatcher(AGGREGATION + " sample(dev.morphia.aggregation.stages.Sample)"),
-        new MethodMatcher(AGGREGATION + " set(dev.morphia.aggregation.stages.Set)"),
-        new MethodMatcher(AGGREGATION + " skip(long)"),
-        new MethodMatcher(AGGREGATION + " sort(dev.morphia.aggregation.stages.Sort)"),
-        new MethodMatcher(AGGREGATION + " sortByCount(dev.morphia.aggregation.stages.SortByCount)"),
-        new MethodMatcher(AGGREGATION + " unionWith(Class,Stage...)"),
-        new MethodMatcher(AGGREGATION + " unionWith(String,Stage...)"),
-        new MethodMatcher(AGGREGATION + " unset(dev.morphia.aggregation.stages.Unset)"),
-        new MethodMatcher(AGGREGATION + " unwind(dev.morphia.aggregation.stages.Unwind)"));
+            new MethodMatcher(AGGREGATION + " addFields(dev.morphia.aggregation.stages.AddFields)"),
+            new MethodMatcher(AGGREGATION + " autoBucket(dev.morphia.aggregation.stages.AutoBucket)"),
+            new MethodMatcher(AGGREGATION + " bucket(dev.morphia.aggregation.stages.Bucket)"),
+            new MethodMatcher(AGGREGATION + " changeStream()"),
+            new MethodMatcher(AGGREGATION + " changeStream(dev.morphia.aggregation.stages.ChangeStream)"),
+            new MethodMatcher(AGGREGATION + " collStats(dev.morphia.aggregation.stages.CollectionStats)"),
+            new MethodMatcher(AGGREGATION + " count(dev.morphia.aggregation.stages.Count)"),
+            new MethodMatcher(AGGREGATION + " currentOp(dev.morphia.aggregation.stages.CountOp)"),
+            new MethodMatcher(AGGREGATION + " densify(dev.morphia.aggregation.stages.Densify)"),
+            new MethodMatcher(AGGREGATION + " documents(dev.morphia.aggregation.expressions.impls.DocumentExpression)"),
+            new MethodMatcher(AGGREGATION + " facet(dev.morphia.aggregation.stages.Facet)"),
+            new MethodMatcher(AGGREGATION + " fill(dev.morphia.aggregation.stages.Fill)"),
+            new MethodMatcher(AGGREGATION + " geoNear(dev.morphia.aggregation.stages.GeoNear)"),
+            new MethodMatcher(AGGREGATION + " graphLookup(dev.morphia.aggregation.stages.GraphLookup)"),
+            new MethodMatcher(AGGREGATION + " group(dev.morphia.aggregation.stages.Group)"),
+            new MethodMatcher(AGGREGATION + " indexStats(dev.morphia.aggregation.stages.IndexStats)"),
+            new MethodMatcher(AGGREGATION + " limit(long)"),
+            new MethodMatcher(AGGREGATION + " lookup(dev.morphia.aggregation.stages.Lookup)"),
+            new MethodMatcher(AGGREGATION + " match(dev.morphia.query.filters.Filter...)"),
+            new MethodMatcher(AGGREGATION + " planCacheStats()"),
+            new MethodMatcher(AGGREGATION + " project(dev.morphia.aggregation.stages.Projection)"),
+            new MethodMatcher(AGGREGATION + " redact(dev.morphia.aggregation.stages.Redact)"),
+            new MethodMatcher(AGGREGATION + " replaceRoot(dev.morphia.aggregation.stages.ReplaceRoot)"),
+            new MethodMatcher(AGGREGATION + " replaceWith(dev.morphia.aggregation.stages.ReplaceWith)"),
+            new MethodMatcher(AGGREGATION + " sample(dev.morphia.aggregation.stages.Sample)"),
+            new MethodMatcher(AGGREGATION + " set(dev.morphia.aggregation.stages.Set)"),
+            new MethodMatcher(AGGREGATION + " skip(long)"),
+            new MethodMatcher(AGGREGATION + " sort(dev.morphia.aggregation.stages.Sort)"),
+            new MethodMatcher(AGGREGATION + " sortByCount(dev.morphia.aggregation.stages.SortByCount)"),
+            new MethodMatcher(AGGREGATION + " unionWith(Class,Stage...)"),
+            new MethodMatcher(AGGREGATION + " unionWith(String,Stage...)"),
+            new MethodMatcher(AGGREGATION + " unset(dev.morphia.aggregation.stages.Unset)"),
+            new MethodMatcher(AGGREGATION + " unwind(dev.morphia.aggregation.stages.Unwind)"));
 
     private static final MethodMatcher MEGA_MATCHER = new MethodMatcher(
-        AGGREGATION + " addFields(dev.morphia.aggregation.stages.AddFields)") {
+            AGGREGATION + " addFields(dev.morphia.aggregation.stages.AddFields)") {
         @Override
         public boolean matches(@Nullable MethodCall methodCall) {
-            return matchers.stream().anyMatch(matcher -> matcher.matches(methodCall)) || PIPELINE.matches(methodCall);
+            return matchers.stream().anyMatch(matcher -> matcher.matches(methodCall));
         }
     };
 
     private static final JavaTemplate PIPELINE_TEMPLATE = (JavaTemplate.builder("Aggregation.pipeline(#{any(List<Stage>)})"))
-                                                              .javaParser(JavaParser.fromJavaVersion()
-                                                                                    .classpath("morphia-core"))
-                                                              .imports(Aggregation.class.getName())
-                                                              .imports(Stage.class.getName())
-                                                              .doBeforeParseTemplate(System.out::println)
-                                                              .build();
+            .javaParser(JavaParser.fromJavaVersion()
+                    .classpath("morphia-core"))
+            .imports(Aggregation.class.getName())
+            .imports(Stage.class.getName())
+            .doBeforeParseTemplate(System.out::println)
+            .build();
 
-    private static final JavaTemplate MATCH = (JavaTemplate.builder("Match.match(#{any()})"))
-                                                  .javaParser(JavaParser.fromJavaVersion()
-                                                                        .classpath("morphia-core"))
-                                                  .imports(Filter.class.getName())
-                                                  .imports(Match.class.getName())
-                                                  .build();
+    private static final JavaTemplate MATCH = (JavaTemplate.builder("Match.match()"))
+            .javaParser(JavaParser.fromJavaVersion()
+                    .classpath("morphia-core"))
+            .imports(Filter.class.getName())
+            .imports(Match.class.getName())
+            .build();
 
     @Override
     public String getDisplayName() {
@@ -98,7 +98,6 @@ public class PipelineRewrite extends Recipe {
     public String getDescription() {
         return "Rewrites an aggregation from using stage-named methods to using pipeline(Stage...).";
     }
-
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
@@ -112,11 +111,10 @@ public class PipelineRewrite extends Recipe {
                         final List<Expression> args = new ArrayList<>();
                         MethodInvocation invocation = (MethodInvocation) updated.getSelect();
                         if (invocation.getSimpleName().equals("match")) {
-
+                            maybeAddImport(Match.class.getName(), "match", false);
                             MethodInvocation applied = MATCH.apply(new Cursor(getCursor(), invocation),
-                                invocation.getCoordinates().replaceMethod(),
-                                invocation.getArguments().toArray());
-                            args.add(applied.withSelect(null));
+                                    invocation.getCoordinates().replaceMethod());
+                            args.add(applied.withArguments(invocation.getArguments()).withSelect(null));
                         } else {
                             args.addAll(invocation.getArguments());
                         }
@@ -124,13 +122,14 @@ public class PipelineRewrite extends Recipe {
                         Space after = methodInvocation.getPadding().getSelect().getAfter();
                         Space space = Space.build(getIndent(after), emptyList());
                         var list = args.stream()
-                                       .map(a -> (Expression) a.withPrefix(space))
-                                       .toList();
+                                .map(a -> (Expression) a.withPrefix(space))
+                                .toList();
 
                         updated = invocation.withArguments(list);
                     }
                     //                    return updated.withName(methodInvocation.getName().withSimpleName("pipeline"));
-                    return updated.withName(methodInvocation.getName().withSimpleName("pipeline"));
+                    return maybeAutoFormat(methodInvocation, updated.withName(methodInvocation.getName().withSimpleName("pipeline"))
+                            .withPrefix(Space.build("\n", emptyList())), context);
                 } else {
                     return super.visitMethodInvocation(methodInvocation, context);
                 }
@@ -160,8 +159,8 @@ public class PipelineRewrite extends Recipe {
                         if (invocation.getSimpleName().equals("match")) {
 
                             MethodInvocation applied = MATCH.apply(new Cursor(getCursor(), invocation),
-                                invocation.getCoordinates().replaceMethod(),
-                                invocation.getArguments().toArray());
+                                    invocation.getCoordinates().replaceMethod(),
+                                    invocation.getArguments().toArray());
                             args.add(applied.withSelect(null));
                         } else {
                             args.addAll(invocation.getArguments());
@@ -172,7 +171,7 @@ public class PipelineRewrite extends Recipe {
 
                     Collections.reverse(args);
                     return updated.withName(methodInvocation.getName().withSimpleName("pipeline"))
-                                  .withArguments(args);
+                            .withArguments(args);
                     //                    MethodInvocation applied = PIPELINE_TEMPLATE.apply(new Cursor(getCursor(), updated),
                     //                            updated.getCoordinates().replaceMethod(), args.toArray() );
                     //                    return autoFormat(applied, context);
--- rewrite/src/test/java/dev/morphia/rewrite/recipes/test/PipelineRewriteTest.java
+++ rewrite/src/test/java/dev/morphia/rewrite/recipes/test/PipelineRewriteTest.java
@@ -19,64 +19,122 @@ public class PipelineRewriteTest extends MorphiaRewriteTest {
     @Test
     void unwrapStageMethods() {
         rewriteRun(java(
-          //language=java
-          """
-            import dev.morphia.aggregation.expressions.ComparisonExpressions;
-            
-            import static dev.morphia.aggregation.expressions.AccumulatorExpressions.sum;
-            import static dev.morphia.aggregation.stages.Group.group;
-            import static dev.morphia.aggregation.stages.Group.id;
-            import static dev.morphia.aggregation.stages.Projection.project;
-            import static dev.morphia.aggregation.expressions.Expressions.field;
-            import static dev.morphia.aggregation.expressions.Expressions.value;
-            import static dev.morphia.aggregation.stages.Sort.sort;
-            import static dev.morphia.query.filters.Filters.eq;
-            
-            import dev.morphia.aggregation.Aggregation;
-            import dev.morphia.query.MorphiaCursor;
-            import org.bson.Document;
-            
-            public class UnwrapTest {
-                public MorphiaCursor<Document> update(Aggregation<?> aggregation) {
-                    return aggregation
-                        .match(eq("author", "Sanderson"))
-                        .group(group(id("author")).field("count", sum(value(1))))
-                        .sort(sort().ascending("1"))
-                        .sort(sort().ascending("2"))
-                        .sort(sort().ascending("3"))
-                        .sort(sort().ascending("4"))
-                        .execute(Document.class);
-                }
-            }""",
-          //language=java
-          """
-      import dev.morphia.aggregation.expressions.ComparisonExpressions;
-      
-      import static dev.morphia.aggregation.expressions.AccumulatorExpressions.sum;
-      import static dev.morphia.aggregation.stages.Group.group;
-      import static dev.morphia.aggregation.stages.Group.id;
-      import static dev.morphia.aggregation.stages.Projection.project;
-      import static dev.morphia.aggregation.expressions.Expressions.field;
-      import static dev.morphia.aggregation.expressions.Expressions.value;
-      import static dev.morphia.aggregation.stages.Sort.sort;
-      import static dev.morphia.query.filters.Filters.eq;
+                //language=java
+                """
+                        import dev.morphia.aggregation.expressions.ComparisonExpressions;
 
-      import dev.morphia.aggregation.Aggregation;
-      import dev.morphia.query.MorphiaCursor;
-      import org.bson.Document;
+                        import static dev.morphia.aggregation.expressions.AccumulatorExpressions.sum;
+                        import static dev.morphia.aggregation.stages.Group.group;
+                        import static dev.morphia.aggregation.stages.Group.id;
+                        import static dev.morphia.aggregation.stages.Projection.project;
+                        import static dev.morphia.aggregation.expressions.Expressions.field;
+                        import static dev.morphia.aggregation.expressions.Expressions.value;
+                        import static dev.morphia.aggregation.stages.Sort.sort;
+                        import static dev.morphia.query.filters.Filters.eq;
 
-      public class UnwrapTest {
-          public MorphiaCursor<Document> update(Aggregation<?> aggregation) {
-              return aggregation
-                  .pipeline(
-                      match(eq("author", "Sanderson")),
-                      group(id("author")).field("count", sum(value(1))),
-                      sort().ascending("1"),
-                      sort().ascending("2"),
-                      sort().ascending("3"),
-                      sort().ascending("4"))
-                  .execute(Document.class);
-          }
-      }"""));
-      }
+                        import dev.morphia.aggregation.Aggregation;
+                        import dev.morphia.query.MorphiaCursor;
+                        import org.bson.Document;
+
+                        public class UnwrapTest {
+                            public MorphiaCursor<Document> update(Aggregation<?> aggregation) {
+                                return aggregation
+                                    .match(eq("author", "Sanderson"))
+                                    .group(group(id("author")).field("count", sum(value(1))))
+                                    .sort(sort().ascending("1"))
+                                    .sort(sort().ascending("2"))
+                                    .sort(sort().ascending("3"))
+                                    .sort(sort().ascending("4"))
+                                    .execute(Document.class);
+                            }
+                        }""",
+                //language=java
+                """
+                        import dev.morphia.aggregation.expressions.ComparisonExpressions;
+
+                        import static dev.morphia.aggregation.expressions.AccumulatorExpressions.sum;
+                        import static dev.morphia.aggregation.stages.Group.group;
+                        import static dev.morphia.aggregation.stages.Group.id;
+                        import static dev.morphia.aggregation.stages.Match.match;
+                        import static dev.morphia.aggregation.stages.Projection.project;
+                        import static dev.morphia.aggregation.expressions.Expressions.field;
+                        import static dev.morphia.aggregation.expressions.Expressions.value;
+                        import static dev.morphia.aggregation.stages.Sort.sort;
+                        import static dev.morphia.query.filters.Filters.eq;
+
+                        import dev.morphia.aggregation.Aggregation;
+                        import dev.morphia.query.MorphiaCursor;
+                        import org.bson.Document;
+
+                        public class UnwrapTest {
+                            public MorphiaCursor<Document> update(Aggregation<?> aggregation) {
+                                return#
+                                        aggregation
+                                                .pipeline(
+                                                        match(eq("author", "Sanderson")),
+                                                        group(id("author")).field("count", sum(value(1))),
+                                                        sort().ascending("1"),
+                                                        sort().ascending("2"),
+                                                        sort().ascending("3"),
+                                                        sort().ascending("4"))
+                                    .execute(Document.class);
+                            }
+                        }""".replace('#', ' ')));
+    }
+
+    @Test
+    public void testWhitespace() {
+        rewriteRun(java(
+                //language=java
+                """
+                        import dev.morphia.aggregation.Aggregation;
+
+                        import static dev.morphia.aggregation.expressions.AccumulatorExpressions.top;
+                        import static dev.morphia.aggregation.expressions.ArrayExpressions.array;
+                        import static dev.morphia.aggregation.expressions.Expressions.field;
+                        import static dev.morphia.aggregation.stages.Group.group;
+                        import static dev.morphia.aggregation.stages.Group.id;
+                        import static dev.morphia.query.Sort.descending;
+                        import static dev.morphia.query.filters.Filters.eq;
+
+                        public class UnwrapTest {
+                            Aggregation<?> aggregation;
+                            public Aggregation<?> testWhitespace() {
+                                return aggregation
+                                           .match(eq("gameId", "G1"))
+                                           .group(group(id(field("gameId")))
+                                                      .field("playerId", top(
+                                                          array(field("playerId"), field("score")),
+                                                          descending("score"))));
+                            }
+                        }
+                        """,
+                //language=java
+                """
+                        import dev.morphia.aggregation.Aggregation;
+
+                        import static dev.morphia.aggregation.expressions.AccumulatorExpressions.top;
+                        import static dev.morphia.aggregation.expressions.ArrayExpressions.array;
+                        import static dev.morphia.aggregation.expressions.Expressions.field;
+                        import static dev.morphia.aggregation.stages.Group.group;
+                        import static dev.morphia.aggregation.stages.Group.id;
+                        import static dev.morphia.aggregation.stages.Match.match;
+                        import static dev.morphia.query.Sort.descending;
+                        import static dev.morphia.query.filters.Filters.eq;
+
+                        public class UnwrapTest {
+                            Aggregation<?> aggregation;
+                            public Aggregation<?> testWhitespace() {
+                                return
+                                        aggregation
+                                                .pipeline(
+                                                        match(eq("gameId", "G1")),
+                                                        group(id(field("gameId")))
+                                                                .field("playerId", top(
+                                                                        array(field("playerId"), field("score")),
+                                                                        descending("score"))));
+                            }
+                        }"""));
+    }
+
 }
