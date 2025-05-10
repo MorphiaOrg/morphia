7e7ee63150c300fd55d92cd803844486edcf1a1b
evanchooly <jlee@antwerkz.com>
2025-05-10 13:40:26 -0400

    remove explicit return

--- core/src/test/java/dev/morphia/test/aggregation/stages/TestDocuments.java
+++ core/src/test/java/dev/morphia/test/aggregation/stages/TestDocuments.java
@@ -12,20 +12,17 @@ import static dev.morphia.aggregation.stages.Lookup.lookup;
 public class TestDocuments extends AggregationTest {
     @Test
     public void testLookup() {
-        testPipeline("5.1.0", "lookup", aggregation -> {
-            return aggregation
-                    .match()
-                    .lookup(lookup()
-                            .localField("zip")
-                            .foreignField("zip_id")
-                            .as("city_state")
-                            .pipeline(
-                                    documents(
-                                            document("zip_id", value(94301))
-                                                    .field("name", value("Palo Alto, CA")),
-                                            document("zip_id", value(10019))
-                                                    .field("name", value("New York, NY")))));
-
-        });
+        testPipeline("5.1.0", "lookup", aggregation -> aggregation
+                .match()
+                .lookup(lookup()
+                        .localField("zip")
+                        .foreignField("zip_id")
+                        .as("city_state")
+                        .pipeline(
+                                documents(
+                                        document("zip_id", value(94301))
+                                                .field("name", value("Palo Alto, CA")),
+                                        document("zip_id", value(10019))
+                                                .field("name", value("New York, NY"))))));
     }
 }
